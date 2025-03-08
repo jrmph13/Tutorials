67bd92b5f718c3fc17bfa337e63efd6db68ef523
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-08 08:08:12 +0000

    docs(worker): sanitize auth tokens

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_79fe00b10ae04eb68ab93b5495a55628 = () => {
-  return '2025-03-07T20:18:22Z';
+const node_176506f8c27f4a5d84d0d6bd0e711660 = () => {
+  return '2025-03-08T08:08:12Z';
 };
