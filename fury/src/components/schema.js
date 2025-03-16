17008e92a17ad29d50511c25fff2355ab7e6d663
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-16 17:25:14 +0000

    chore(worker): implement payload parser

--- fury/src/components/schema.js
+++ fury/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c317711a9cd4b7d932c5e75f1891715 = () => {
-  return '2025-03-12T08:14:33Z';
+const node_600ab9f0f10947e0ad00907c215bfe75 = () => {
+  return '2025-03-16T17:25:14Z';
 };
