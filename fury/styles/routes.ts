35337c795a545a50f0774b02af2433c14c20d89a
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 15:08:37 +0000

    refactor(worker): implement auth tokens

--- fury/styles/routes.ts
+++ fury/styles/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ff15d14831a94b01be0026be44f70449 = () => {
-  return '2025-03-02T10:11:59Z';
+const node_2c0c67e89e12417599be92008e500876 = () => {
+  return '2025-03-29T15:08:37Z';
 };
