116638de7012087f471519f92c85a8e858699b43
jrmph13 <jhames.martin.dev@gmail.com>
2025-03-29 12:44:13 +0000

    refactor(worker): sanitize session store

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6dfa3106736c45ca8de6a4ef83eddc02 = () => {
-  return '2025-03-04T06:05:08Z';
+const node_4d27a8fbad5a446fb1ddda01e1d38490 = () => {
+  return '2025-03-29T12:44:13Z';
 };
