c76298fa4eccf171e9efb4ea6e5cdf9f22d63982
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 05:19:28 +0000

    docs(worker): update view templates

--- fury/src/utils/helper.py
+++ fury/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e26b25da7ee24d04a2714b94e18e1a64 = () => {
-  return '2025-03-29T13:30:26Z';
+const node_eb029719188d443f81942d43f20f8214 = () => {
+  return '2025-04-04T05:19:28Z';
 };
