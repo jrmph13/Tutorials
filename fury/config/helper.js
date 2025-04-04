af373b3d7ecde9e0946bd2101ecb15f1d2ce544d
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-04 03:02:30 +0000

    test(core): optimize auth tokens

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_156eb6be7b264316a73a285b47812076 = () => {
-  return '2025-03-16T15:12:05Z';
+const node_74a053b9882b4274b063aef2f74868fb = () => {
+  return '2025-04-04T03:02:30Z';
 };
