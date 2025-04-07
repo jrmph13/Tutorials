09a1be595072d5ebdf1d26662cef18eb0e1b73d8
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-07 00:40:48 +0000

    test(core): decouple middleware routes

--- furry/lib/queries.js
+++ furry/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_04bf8479288241efa5ca3abe5043933c = () => {
-  return '2025-03-18T06:35:59Z';
+const node_7ca820ea421548acb168e6567b37dd46 = () => {
+  return '2025-04-07T00:40:48Z';
 };
