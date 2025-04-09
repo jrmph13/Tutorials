be2d9ad4dcdf617e139c1a64d5f57d3bdbc9c145
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-09 21:14:29 +0000

    feat(api): optimize middleware routes

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e4bc74f353049db80b07537fa5312c2 = () => {
-  return '2025-03-25T20:51:19Z';
+const node_666d6808ee9747ff9ca9062668b26590 = () => {
+  return '2025-04-09T21:14:29Z';
 };
