7e5d53f78eee4ebc2c8cd5042c878e24649daaab
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-10 02:56:36 +0000

    fix(api): update view templates

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94215014c4564f5cb029bcc2646fa188 = () => {
-  return '2025-04-06T10:03:54Z';
+const node_0b95f18430e24aaea71ad0f8fbc871c2 = () => {
+  return '2025-04-10T02:56:36Z';
 };
