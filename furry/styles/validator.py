3b3e7da6d34970e5df543188961800452ec8ffbd
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-11 16:51:33 +0000

    refactor(api): sanitize view templates

--- furry/styles/validator.py
+++ furry/styles/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c76143fe0f44cd99934a78f66414979 = () => {
-  return '2025-04-02T07:22:50Z';
+const node_d18d76d86d774959a571834c334ef13c = () => {
+  return '2025-04-11T16:51:33Z';
 };
