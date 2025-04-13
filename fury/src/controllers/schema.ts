26efb361f14191fa765e1d78012501bdfa0dc37b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-13 11:19:01 +0000

    test(core): integrate auth tokens

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2ad2247419564bac8feadebf3d12fdcc = () => {
-  return '2025-03-18T11:09:41Z';
+const node_888f4f016f904d26a879b0212ccac405 = () => {
+  return '2025-04-13T11:19:01Z';
 };
