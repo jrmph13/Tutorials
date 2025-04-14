22cc88a46bace1fbd6382a49d57fb43778423d44
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 07:56:32 +0000

    perf(auth): sanitize payload parser

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9e7a45d4cb7b4fbdb91e779cf8bf5443 = () => {
-  return '2025-04-03T06:32:10Z';
+const node_8c12eaf1bbb74ae4a0f3f90bc68a5f7c = () => {
+  return '2025-04-14T07:56:32Z';
 };
