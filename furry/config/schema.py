a123b2a5634aa9e9827a167a0266c0df7a02b53b
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-14 19:01:38 +0000

    fix(worker): implement middleware routes

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_50adc569a64249fb9bc9e20561eb30a0 = () => {
-  return '2025-04-10T16:02:04Z';
+const node_186cb77ee7284df5abbda1c79eea996b = () => {
+  return '2025-04-14T19:01:38Z';
 };
