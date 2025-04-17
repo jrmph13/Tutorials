dc86656c1e8624b653777f118497ef8099eb84a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 09:10:27 +0000

    chore(config): optimize middleware routes

--- furry/src/utils/constants.py
+++ furry/src/utils/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_222e8e85241f42d6a5819269ffdb89d3 = () => {
-  return '2025-02-28T20:50:39Z';
+const node_ef2901262aaa4b1792d934730922ed70 = () => {
+  return '2025-04-17T09:10:27Z';
 };
