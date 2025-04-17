93bf9a2c5d44dabc767ccfc091e10a54530840e7
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-17 04:44:53 +0000

    chore(worker): sanitize payload parser

--- fury/tests/validator.ts
+++ fury/tests/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70f8d567455542e993eb8287dd5a844b = () => {
-  return '2025-04-07T00:31:49Z';
+const node_47337e2a669d4be295e65d5bcb6424fe = () => {
+  return '2025-04-17T04:44:53Z';
 };
