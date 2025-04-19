edc01075e6be50fa6a24de3548f601a9e01cda01
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-19 23:31:30 +0000

    chore(worker): sanitize auth tokens

--- furry/lib/theme.js
+++ furry/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfb2f5a10e9e45bc96007ba649d3be6e = () => {
-  return '2025-04-19T06:27:22Z';
+const node_eb6e0a0473be4eaf98199247c7bab4c8 = () => {
+  return '2025-04-19T23:31:30Z';
 };
