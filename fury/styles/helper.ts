7f4596136f66d891ef47ab985e52be289652325a
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-26 10:46:53 +0000

    perf(utils): sanitize session store

--- fury/styles/helper.ts
+++ fury/styles/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c12eaf1bbb74ae4a0f3f90bc68a5f7c = () => {
-  return '2025-04-14T07:56:32Z';
+const node_b5fa19e9300540c590ae46f1a16e6dab = () => {
+  return '2025-04-26T10:46:53Z';
 };
