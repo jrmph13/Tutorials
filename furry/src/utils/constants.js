dadd48e4aa9d3d5a73f57d129113899033ee2f5f
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-27 18:45:21 +0000

    style(auth): restructure payload parser

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f68eb3e92f0341a48ed728ab639468e2 = () => {
-  return '2025-04-24T19:23:17Z';
+const node_eba5c4b4299c493b8336def57f023ca2 = () => {
+  return '2025-04-27T18:45:21Z';
 };
