6a613311524883ab312643607713538f13d6a833
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-29 14:58:25 +0000

    test(ui): sanitize auth tokens

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e7ef4cadfa684ccc9792a5dcd9aba471 = () => {
-  return '2025-03-20T07:16:59Z';
+const node_71b2115b062845939bd6e5061824138c = () => {
+  return '2025-04-29T14:58:25Z';
 };
