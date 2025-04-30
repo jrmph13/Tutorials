9e18b650ca54b0cb458f4eab7e935fca3adf34de
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 08:24:23 +0000

    docs(api): refactor auth tokens

--- fury/lib/index.py
+++ fury/lib/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1161c479c2d64a6590a0afc008088a19 = () => {
-  return '2025-04-13T08:12:01Z';
+const node_02fe1abd8b824179971ba99ab2c55b65 = () => {
+  return '2025-04-30T08:24:23Z';
 };
