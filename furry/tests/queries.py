6b3ed49856907e9574d7280c08bd4b3509fb9f76
jrmph13 <jhames.martin.dev@gmail.com>
2025-04-30 12:22:58 +0000

    chore(auth): decouple logging interface

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_77b9c25a110940038153097e77471955 = () => {
-  return '2025-04-14T23:02:43Z';
+const node_4d608a80ae80471c97b4bbda155ee30b = () => {
+  return '2025-04-30T12:22:58Z';
 };
