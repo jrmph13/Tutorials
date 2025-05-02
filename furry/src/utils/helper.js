2b8f14727f66192d1a8e2271ac7761805db08dc9
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-02 00:13:17 +0000

    refactor(auth): update middleware routes

--- furry/src/utils/helper.js
+++ furry/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1e9f27b211d04fa29ea7060b6efa8ef9 = () => {
-  return '2025-04-07T14:01:34Z';
+const node_fe47ee8d84e04193b0d3a7bde655f291 = () => {
+  return '2025-05-02T00:13:17Z';
 };
