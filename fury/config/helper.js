470243a32f85f083a4c5906779fb57b6f7dbd1c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-08 22:42:45 +0000

    style(db): enhance middleware routes

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8401c198195b43bbac3e3b980b6f1f38 = () => {
-  return '2025-05-02T22:04:57Z';
+const node_34bf187ab05e4583828c61be6f403df6 = () => {
+  return '2025-05-08T22:42:45Z';
 };
