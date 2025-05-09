1e5c940c0a9a3e0b62664899aeea3e3863b34736
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-09 01:53:29 +0000

    perf(ui): optimize view templates

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7d3e7b9d4ae647e7aa4fcb133def5a2f = () => {
-  return '2025-04-16T16:54:05Z';
+const node_ff749b972668408588522ff55c97dc9e = () => {
+  return '2025-05-09T01:53:29Z';
 };
