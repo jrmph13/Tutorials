55c203d1b77a7567e07e8b55ef8a03038d395341
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-10 12:56:33 +0000

    style(core): decouple database index

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b554aca7b5ca42df9ed2f3c05f7e5bb9 = () => {
-  return '2025-04-27T22:52:48Z';
+const node_6089e444a40745da97f4740a832a373b = () => {
+  return '2025-05-10T12:56:33Z';
 };
