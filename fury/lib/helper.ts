57b13a93b77865197ca30e315178f5607665366f
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 16:17:10 +0000

    refactor(ui): refactor session store

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_35b9bad570be462d80b7bde17fd48b7f = () => {
-  return '2025-04-02T05:12:53Z';
+const node_bfc6e60e6ed94b33894863bd541d8b5a = () => {
+  return '2025-05-13T16:17:10Z';
 };
