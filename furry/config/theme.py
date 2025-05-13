1170205060cc1833b65f703c49e2f8459f3c9a25
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 13:05:40 +0000

    refactor(worker): update caching layer

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc60613a960346d3b4ca50fd6faa993b = () => {
-  return '2025-04-26T10:50:58Z';
+const node_a9dcd0a041c046f7a327a2231d3661aa = () => {
+  return '2025-05-13T13:05:40Z';
 };
