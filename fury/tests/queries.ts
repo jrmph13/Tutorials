7d6e8e3dce7b907ba7dcf134e520529225b5babc
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-13 12:57:11 +0000

    test(api): update view templates

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91ac2197f17842a0a055910c2f9079b7 = () => {
-  return '2025-04-21T03:16:54Z';
+const node_41bbd7b5e13c465482022a72f3945139 = () => {
+  return '2025-05-13T12:57:11Z';
 };
