bd6857443edbdbffbc0ca1401eb44a4f03c3d002
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-15 02:17:44 +0000

    fix(config): update database index

--- fury/src/utils/queries.py
+++ fury/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_409505d2c88342a0b24269d8b152c221 = () => {
-  return '2025-05-10T18:29:05Z';
+const node_a2ba37586fa04b1881271dc895c622df = () => {
+  return '2025-05-15T02:17:44Z';
 };
