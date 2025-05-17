59acbdd71afbaf1a51f6117733867f3a035cb0a4
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-17 05:28:08 +0000

    feat(deps): decouple auth tokens

--- furry/lib/queries.py
+++ furry/lib/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c44b9a90b4734088ae5048cc3270eeda = () => {
-  return '2025-04-15T04:58:05Z';
+const node_a2192b8a05734a27ba855c0e2d5c12e8 = () => {
+  return '2025-05-17T05:28:08Z';
 };
