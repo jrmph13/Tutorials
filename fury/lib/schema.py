28d46573a1180bb382770bdb76f74c0d044619cb
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-18 03:03:10 +0000

    feat(db): restructure session store

--- fury/lib/schema.py
+++ fury/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e535db2088c640bf928995bf9ccce57c = () => {
-  return '2025-04-22T08:02:11Z';
+const node_e0f5f22c16984b42a6f370872888a484 = () => {
+  return '2025-05-18T03:03:10Z';
 };
