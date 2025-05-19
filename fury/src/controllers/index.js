8175a82015be5ac538944cd2af312d6eec6ccf5a
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 16:12:56 +0000

    test(auth): optimize view templates

--- fury/src/controllers/index.js
+++ fury/src/controllers/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5f94a9d85b47446bb7cd2d160d83cd0a = () => {
-  return '2025-05-02T21:32:58Z';
+const node_4a78dbd4cc764145832955916432cddf = () => {
+  return '2025-05-19T16:12:56Z';
 };
