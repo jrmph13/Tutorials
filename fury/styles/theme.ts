57e70b59cdf082e46d1281beb0b50f63f1541539
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-19 19:06:02 +0000

    fix(utils): update session store

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_17018ba631514e5dbb5e764607d990fa = () => {
-  return '2025-04-19T22:50:59Z';
+const node_e5a096bf8c604224b762ec17dac07021 = () => {
+  return '2025-05-19T19:06:02Z';
 };
