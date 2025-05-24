b01241e095401e919d99f4f93b6ccc13178ea8ce
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-24 20:27:53 +0000

    test(config): refactor auth tokens

--- fury/src/controllers/queries.py
+++ fury/src/controllers/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3026b04d656844a8b4d3ba3354782f5f = () => {
-  return '2025-05-11T14:58:58Z';
+const node_7e9f930c4d654fcab5d2bf47d461e32b = () => {
+  return '2025-05-24T20:27:53Z';
 };
