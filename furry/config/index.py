56d1dca4b36871d9901b670839b1562db871296b
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 11:06:22 +0000

    test(db): fix view templates

--- furry/config/index.py
+++ furry/config/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a2c30bff4e040439fba14d4825f7839 = () => {
-  return '2025-02-24T14:04:48Z';
+const node_d66db0cade9f43a6851757562edb070a = () => {
+  return '2025-05-25T11:06:22Z';
 };
