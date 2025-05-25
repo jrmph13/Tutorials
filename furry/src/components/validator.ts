f88fccad3be7ca9dd88714e8108547ea601a0361
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-25 05:57:27 +0000

    refactor(api): sanitize auth tokens

--- furry/src/components/validator.ts
+++ furry/src/components/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66d9fcf7e3334c73a2b4ddcc114889fb = () => {
-  return '2025-05-08T20:35:33Z';
+const node_d193ddb1185244518122fbf04a7bbb49 = () => {
+  return '2025-05-25T05:57:27Z';
 };
