45f70319f1bc02190f4569205f59b6d81854d0c8
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-26 22:54:42 +0000

    docs(utils): decouple middleware routes

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c06a2cad182e4c9b80d42a49830adbbd = () => {
-  return '2025-05-10T13:55:24Z';
+const node_dbfab8828ef245ecadbf549d9d2a5128 = () => {
+  return '2025-05-26T22:54:42Z';
 };
