18cf1217687ff4e0465fae1ab29c3db84a3f80e6
jrmph13 <jhames.martin.dev@gmail.com>
2025-05-28 21:08:43 +0000

    fix(auth): refactor validation pipeline

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81b1e0b55aa042e6a8ec31aba068b04c = () => {
-  return '2025-05-26T09:39:42Z';
+const node_40d520874acc4dd980c897afc9c7c6b1 = () => {
+  return '2025-05-28T21:08:43Z';
 };
