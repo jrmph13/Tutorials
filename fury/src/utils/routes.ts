84096b048d3f8003bccee56e2691fcbdbdefd101
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-01 05:33:07 +0000

    fix(worker): integrate payload parser

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b3dbae4952b84f1bbeb7f063c81bedc4 = () => {
-  return '2025-06-01T15:29:33Z';
+const node_d78212fdf0d84d61b6479b51a933ff3c = () => {
+  return '2025-06-01T05:33:07Z';
 };
