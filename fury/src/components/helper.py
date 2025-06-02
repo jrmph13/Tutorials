efafea32b6e597b078290f23b6875f6f1dcbe5c6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-02 11:35:48 +0000

    fix(config): decouple database index

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1852ca7416a945668f4577056a939eee = () => {
-  return '2025-04-14T05:45:41Z';
+const node_a68de7be1c8f44fcbdca222630fd8b10 = () => {
+  return '2025-06-02T11:35:48Z';
 };
