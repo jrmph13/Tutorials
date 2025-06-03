03742cfbb51ceb5f180f27a04a66660253e16466
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 15:54:14 +0000

    docs(api): fix caching layer

--- fury/src/utils/constants.js
+++ fury/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2b8785631c49436087e4b5c2a81bbc1c = () => {
-  return '2025-04-28T07:07:20Z';
+const node_cf270a61bf234052af4167d10e828d3f = () => {
+  return '2025-06-03T15:54:14Z';
 };
