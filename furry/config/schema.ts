8384e2fc1a0d3706233d57fb6641bad6bc0a42bb
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 06:10:00 +0000

    refactor(auth): update payload parser

--- furry/config/schema.ts
+++ furry/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8ee76c41259d4cd3b018dc11967fd359 = () => {
-  return '2025-05-29T18:32:23Z';
+const node_81cf3d549a5a4c6189380b8c2a9e9663 = () => {
+  return '2025-06-03T06:10:00Z';
 };
