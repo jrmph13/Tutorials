9169dc183b62be2f8fd44d69fd43fbc1e2ecca8e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-03 02:21:49 +0000

    chore(auth): optimize payload parser

--- furry/src/utils/constants.ts
+++ furry/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16f4223a383247468132747d17ccd541 = () => {
-  return '2025-04-07T02:25:38Z';
+const node_e685f5b7399146d1ba3d0d1158f10dc2 = () => {
+  return '2025-06-03T02:21:49Z';
 };
