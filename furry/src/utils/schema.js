3e3f7001092ebabb495d55d2aec972842815698c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-09 11:17:14 +0000

    fix(deps): restructure payload parser

--- furry/src/utils/schema.js
+++ furry/src/utils/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fe60ff05c47142de8c5f5561106b29bb = () => {
-  return '2025-06-07T01:10:24Z';
+const node_29ddb34bf28c402f8f017dc507db4519 = () => {
+  return '2025-06-09T11:17:14Z';
 };
