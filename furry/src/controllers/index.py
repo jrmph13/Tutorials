07672300abb4cdf38cea1abd33002ce89dd12108
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-10 10:31:01 +0000

    perf(auth): sanitize view templates

--- furry/src/controllers/index.py
+++ furry/src/controllers/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83cace6f7f8a4c0d9ec4bcc1774cce65 = () => {
-  return '2025-04-27T05:12:12Z';
+const node_7388ee788afe409b9bcf7205ac8368f7 = () => {
+  return '2025-06-10T10:31:01Z';
 };
