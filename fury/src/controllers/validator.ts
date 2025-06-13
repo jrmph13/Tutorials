8dd905979789b13b50920b7b11cb0536107546cf
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-13 13:23:13 +0000

    test(ui): restructure payload parser

--- fury/src/controllers/validator.ts
+++ fury/src/controllers/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_611d5aef94174dcf931f069d95bc5564 = () => {
-  return '2025-05-22T10:41:11Z';
+const node_450bfee1dbb34255a157dfecc07d9f71 = () => {
+  return '2025-06-13T13:23:13Z';
 };
