ce953040f120c6b7b013696e9b0574b694c5f58e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 03:54:26 +0000

    test(core): restructure payload parser

--- fury/src/utils/schema.ts
+++ fury/src/utils/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e42574f27e764955827aac36cef78a06 = () => {
-  return '2025-05-13T16:57:34Z';
+const node_76ef1e7e6d8b414db8268b093652d22f = () => {
+  return '2025-06-14T03:54:26Z';
 };
