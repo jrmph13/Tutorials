6245cf982500faef3d3db927dc592cdc22f4b58e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-14 17:00:28 +0000

    perf(deps): sanitize database index

--- furry/config/schema.js
+++ furry/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_65b730b9a5fb4dd7bff54104f3ee4263 = () => {
-  return '2025-06-08T05:13:55Z';
+const node_a58110bc545b4495b7fe770364a60fdd = () => {
+  return '2025-06-14T17:00:28Z';
 };
