37a0c4014a5c491db22ec40e52a35236acef1b91
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 12:13:47 +0000

    test(deps): enhance auth tokens

--- fury/src/components/routes.py
+++ fury/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4a54ec66d51142be8ca633fddaf41c7f = () => {
-  return '2025-03-29T21:09:56Z';
+const node_7f3ad9801dde44eca153695b489840ae = () => {
+  return '2025-06-15T12:13:47Z';
 };
