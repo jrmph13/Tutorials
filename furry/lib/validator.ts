ead731fcba7da5aa266ada523090f4ce88e4a193
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-15 08:22:21 +0000

    style(config): update auth tokens

--- furry/lib/validator.ts
+++ furry/lib/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fb0b717621234c7d8a8fe6363754c336 = () => {
-  return '2025-05-18T02:50:59Z';
+const node_8107902c02e84c5cbeda4972f782237e = () => {
+  return '2025-06-15T08:22:21Z';
 };
