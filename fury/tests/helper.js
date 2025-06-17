68f6970b73bb68f376a7a829bd726781076b8250
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 20:03:25 +0000

    perf(worker): optimize validation pipeline

--- fury/tests/helper.js
+++ fury/tests/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6ef93c7c99dd4abf9dccfbab782a8633 = () => {
-  return '2025-04-21T09:17:11Z';
+const node_dd8403e503104179820ba8733d46efb9 = () => {
+  return '2025-06-17T20:03:25Z';
 };
