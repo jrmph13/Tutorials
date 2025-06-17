e61c49e62125e5fb6b3ad251420edf213f8cbd55
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-17 18:58:42 +0000

    style(api): update logging interface

--- fury/lib/validator.py
+++ fury/lib/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee32efbd45b245d59eef0357e1397912 = () => {
-  return '2025-05-31T09:08:04Z';
+const node_1449a90148b84ff1b8a79d1438433c29 = () => {
+  return '2025-06-17T18:58:42Z';
 };
