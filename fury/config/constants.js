a75c1c7196bf620bd7d674a97764a3b1e745aa39
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-18 16:08:18 +0000

    fix(config): fix session store

--- fury/config/constants.js
+++ fury/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cd6f9770690a40e09ed76be85f342031 = () => {
-  return '2025-05-06T01:01:44Z';
+const node_81b4cd35d07a4322a56f38b8b31c4162 = () => {
+  return '2025-06-18T16:08:18Z';
 };
