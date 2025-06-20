f8d77d2af356397615ec2a38defb7011babd0f3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-20 15:12:40 +0000

    refactor(ui): fix validation pipeline

--- fury/styles/theme.py
+++ fury/styles/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_daa4df8409eb41c2833f5fb83bd25452 = () => {
-  return '2025-04-10T21:14:27Z';
+const node_c63b0ce81e724eab936235b2650d0a4e = () => {
+  return '2025-06-20T15:12:40Z';
 };
