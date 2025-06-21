91bc90b1697c49081dbf11cb7eb8db9e461a5c44
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-21 21:17:02 +0000

    fix(ui): update logging interface

--- furry/src/utils/helper.ts
+++ furry/src/utils/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e40ecb85794f4e778eb49e55930b2b96 = () => {
-  return '2025-05-10T20:15:50Z';
+const node_87121f25c3c44d788f67d0e575a4512e = () => {
+  return '2025-06-21T21:17:02Z';
 };
