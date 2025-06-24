d4c54cb2cfa94707e598a907ffd64c8e1ec4e8ba
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-24 22:40:14 +0000

    refactor(worker): restructure payload parser

--- furry/tests/constants.js
+++ furry/tests/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5e6e10e96ff4195b88962523454ef50 = () => {
-  return '2025-06-10T16:32:29Z';
+const node_a4da059dbff44a7ab99e06833e5a7492 = () => {
+  return '2025-06-24T22:40:14Z';
 };
