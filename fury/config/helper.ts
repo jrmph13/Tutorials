7991f2b921803e1dbe939bfe17e9c941508ff214
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-25 12:42:54 +0000

    docs(worker): decouple payload parser

--- fury/config/helper.ts
+++ fury/config/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a9fa87d561944fc4a03e335254c4273e = () => {
-  return '2025-05-01T08:18:07Z';
+const node_e04a617996ad42f1bf5e5a01fee9c047 = () => {
+  return '2025-06-25T12:42:54Z';
 };
