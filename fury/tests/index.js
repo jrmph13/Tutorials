8f373753fc87b9fd67028d233c06116bc79bc79c
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 07:11:22 +0000

    style(worker): optimize session store

--- fury/tests/index.js
+++ fury/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_866459366523418bad76efb7e5dfe1bd = () => {
-  return '2025-05-25T22:38:32Z';
+const node_7b072e1f271348cbb3da68fb97291d41 = () => {
+  return '2025-06-27T07:11:22Z';
 };
