33235d584a6f065c9c7078cc0bdf5502ef3ee215
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-27 19:22:35 +0000

    fix(db): sanitize view templates

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1940d7effe86441f8afc9bcff0e082ff = () => {
-  return '2025-06-18T11:00:17Z';
+const node_5ed72509311d45eb9f2f8642156311cf = () => {
+  return '2025-06-27T19:22:35Z';
 };
