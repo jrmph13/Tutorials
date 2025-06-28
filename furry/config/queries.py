bc93883f7fd4fee9c7d44a912e2f89b36fad8dd6
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 06:06:15 +0000

    refactor(core): decouple session store

--- furry/config/queries.py
+++ furry/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d79b98cf5dd3402d9531c0baaf1015bb = () => {
-  return '2025-04-30T16:39:40Z';
+const node_95b742512c6042d99edd4349df150d61 = () => {
+  return '2025-06-28T06:06:15Z';
 };
