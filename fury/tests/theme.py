4c6b93be8611f0359386100e263d46edbbc54ee0
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-28 23:47:22 +0000

    feat(auth): enhance auth tokens

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ace39d7926af4421bc764151b6d05861 = () => {
-  return '2025-06-24T00:49:31Z';
+const node_681ff70c533b4e60891bfc5497acf039 = () => {
+  return '2025-06-28T23:47:22Z';
 };
