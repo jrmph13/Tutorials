a0d1802f8758d047b75872b5b7af7be2588f5a96
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 02:31:02 +0000

    refactor(core): enhance logging interface

--- fury/styles/schema.ts
+++ fury/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b925a0e7d5694149a3a406b1e5ee7ad7 = () => {
-  return '2025-04-22T17:08:36Z';
+const node_3755045d47d646e786c8839f0ee66291 = () => {
+  return '2025-06-30T02:31:02Z';
 };
