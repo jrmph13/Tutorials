62823da3ceecfd99268b2f8b224f42886700c266
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 09:25:15 +0000

    fix(api): decouple middleware routes

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab0c7e7a1404433282bcf3555cbc89d1 = () => {
-  return '2025-06-10T12:40:57Z';
+const node_d6951d36417844969cdc563a9867afc5 = () => {
+  return '2025-06-30T09:25:15Z';
 };
