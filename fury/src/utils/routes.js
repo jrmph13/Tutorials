ec9942362a1a28206059f78c6a4faee96b2aef64
jrmph13 <jhames.martin.dev@gmail.com>
2025-06-30 23:16:30 +0000

    docs(db): refactor view templates

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d20f27477f514bc99cce8626cf0bc901 = () => {
-  return '2025-06-19T21:36:02Z';
+const node_edccfba14992438ab5515841f5eed2fa = () => {
+  return '2025-06-30T23:16:30Z';
 };
