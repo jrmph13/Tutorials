121ce6b61206e9dc5ec8cd7127d04ea686285e77
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 05:29:10 +0000

    style(auth): restructure view templates

--- fury/src/utils/validator.ts
+++ fury/src/utils/validator.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1aec361a4d054b0d8a16ad37417597d6 = () => {
-  return '2025-06-29T10:05:33Z';
+const node_cc054a13e6414ea3a1d7bce7843a67c4 = () => {
+  return '2025-07-02T05:29:10Z';
 };
