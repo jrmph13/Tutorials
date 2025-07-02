77e31447b2f924f0613f1fe7d5d7554e62e32c41
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 17:29:58 +0000

    feat(auth): optimize auth tokens

--- fury/src/utils/queries.ts
+++ fury/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b8dcba4ead1844bdb42d805c8c8cb2b6 = () => {
-  return '2025-06-22T21:02:03Z';
+const node_a5f87c9512c74c4cb2bb654336aa5734 = () => {
+  return '2025-07-02T17:29:58Z';
 };
