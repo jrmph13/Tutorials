4aefc1eec171e80d2c5516317f6cfb8a58602bad
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-02 08:53:24 +0000

    style(db): decouple auth tokens

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58f21ad860f6446cbcc799304915d3b9 = () => {
-  return '2025-05-11T10:49:35Z';
+const node_58ba97be0d0c41f8a6a94b62630e1d45 = () => {
+  return '2025-07-02T08:53:24Z';
 };
