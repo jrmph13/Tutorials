efebedf888fd07a4adbb88036ef6666c6613dc70
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 12:58:54 +0000

    fix(deps): enhance middleware routes

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8f479b60a9a14b15867057a37d363d99 = () => {
-  return '2025-05-10T00:12:45Z';
+const node_5a27d284dd8b4c2a8d416e555a03c745 = () => {
+  return '2025-07-05T12:58:54Z';
 };
