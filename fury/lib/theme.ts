efe3fa80b4121c47f96e7e96234f3c07512cfafe
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-05 01:39:43 +0000

    style(deps): fix database index

--- fury/lib/theme.ts
+++ fury/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9bc4b9ebe8a54d17952874d33c911488 = () => {
-  return '2025-06-26T22:06:59Z';
+const node_c3572b9057bf45488ce816af474d3876 = () => {
+  return '2025-07-05T01:39:43Z';
 };
