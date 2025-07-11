7aef460687ef791822530b9a3a765c0d854b12e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-11 00:40:11 +0000

    test(auth): update middleware routes

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d67d3d1c5f51403ca1c12866ca2ea20a = () => {
-  return '2025-06-07T23:51:28Z';
+const node_afca3e3087eb43e79ed563b9347187fb = () => {
+  return '2025-07-11T00:40:11Z';
 };
