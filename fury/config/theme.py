6545e6831a60709732d42710e62c4a50e1afb624
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-12 12:22:25 +0000

    perf(worker): optimize logging interface

--- fury/config/theme.py
+++ fury/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_300a0c3d4d20449c9e886ce1408fe34d = () => {
-  return '2025-04-04T08:26:10Z';
+const node_ee192c0e7d4b44b4913b3aab295339c6 = () => {
+  return '2025-07-12T12:22:25Z';
 };
