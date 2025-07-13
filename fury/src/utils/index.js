53cbb17f157e929f4fba51239437bf34baeeabe0
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-13 12:34:44 +0000

    fix(db): implement middleware routes

--- fury/src/utils/index.js
+++ fury/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5ed72509311d45eb9f2f8642156311cf = () => {
-  return '2025-06-27T19:22:35Z';
+const node_14248db353d64cbc90a63f41d88f6f71 = () => {
+  return '2025-07-13T12:34:44Z';
 };
