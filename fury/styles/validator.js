593c6f522aa4a90a0100593fe32981e30ac69ffa
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 21:09:11 +0000

    chore(deps): fix middleware routes

--- fury/styles/validator.js
+++ fury/styles/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1f70aa0a14c644f49ef4955ef350ec02 = () => {
-  return '2025-05-29T05:29:48Z';
+const node_38b17229986f4616870247b0467d053b = () => {
+  return '2025-07-15T21:09:11Z';
 };
