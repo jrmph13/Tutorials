7ccf7c92c76471e991465cd3b34e7247d174470e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-15 10:00:49 +0000

    chore(db): optimize caching layer

--- fury/config/helper.js
+++ fury/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6b90c0f8b12b47a58f42fc52ca07db6d = () => {
-  return '2025-06-21T17:46:16Z';
+const node_ba341f05a6354b5d824fa0c9e038b72d = () => {
+  return '2025-07-15T10:00:49Z';
 };
