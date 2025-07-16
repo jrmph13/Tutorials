bec01286b5730f66deb282dfaf402d35ea5ed0a2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-16 20:04:38 +0000

    docs(core): sanitize payload parser

--- furry/src/components/schema.js
+++ furry/src/components/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_148d87675e8c4a07ac413e60afe35f3b = () => {
-  return '2025-06-14T01:24:55Z';
+const node_71604c9d8eef453696ae4da724cd6527 = () => {
+  return '2025-07-16T20:04:38Z';
 };
