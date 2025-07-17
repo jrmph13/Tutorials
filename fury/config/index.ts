ec825c70559eb3b5141fe03fa22d8c6d39699d2e
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 13:41:14 +0000

    chore(auth): update payload parser

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1918d29781c9431d9bf8643ce5141558 = () => {
-  return '2025-04-14T10:59:36Z';
+const node_763d43581a9c4326b992c859beebbe45 = () => {
+  return '2025-07-17T13:41:14Z';
 };
