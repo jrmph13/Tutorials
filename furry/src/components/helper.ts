115749e91192417ec44281fb0651877934c0f161
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-17 00:41:30 +0000

    style(auth): implement auth tokens

--- furry/src/components/helper.ts
+++ furry/src/components/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f3661400d27141c790e46798f7107535 = () => {
-  return '2025-07-15T23:26:18Z';
+const node_59ad2cf5d5c14f33bc2d1eda3a47ad7b = () => {
+  return '2025-07-17T00:41:30Z';
 };
