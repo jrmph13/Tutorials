176618d35fa69edfe362308fa2f1b0a2f2979c77
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-18 23:07:53 +0000

    chore(db): update auth tokens

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0305ba6bc55b48d39777951d4734a623 = () => {
-  return '2025-06-15T21:24:38Z';
+const node_7df04f8f64d1424b87a91e78f60115ba = () => {
+  return '2025-07-18T23:07:53Z';
 };
