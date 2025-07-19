0bc23d8df4b2678325388a73a539d95ffd4dd365
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 20:13:37 +0000

    perf(db): fix database index

--- furry/src/utils/queries.py
+++ furry/src/utils/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a02c1c2e913d473ca00c2f7f1326f2db = () => {
-  return '2025-05-15T14:55:58Z';
+const node_1befe2e8550f49d59154000f81222352 = () => {
+  return '2025-07-19T20:13:37Z';
 };
