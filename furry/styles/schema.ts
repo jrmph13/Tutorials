6cda9f26ecda0b0eeaa2025da3c73bb037da1584
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-19 09:40:38 +0000

    chore(core): decouple view templates

--- furry/styles/schema.ts
+++ furry/styles/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91d4e775037848f795e5dfe637b3efa0 = () => {
-  return '2025-07-17T13:44:22Z';
+const node_18b7773f7a6141b88778252a639f681b = () => {
+  return '2025-07-19T09:40:38Z';
 };
