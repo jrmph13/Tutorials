5b18ce3c05d31c763a7d5d352c1591be95e4e184
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-24 02:32:03 +0000

    fix(config): sanitize logging interface

--- furry/config/routes.ts
+++ furry/config/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_16fb6aa68b384af5a275c5155b9bdfb9 = () => {
-  return '2025-06-06T23:06:54Z';
+const node_3fa33b08dae94b7e920c39041df9133a = () => {
+  return '2025-07-24T02:32:03Z';
 };
