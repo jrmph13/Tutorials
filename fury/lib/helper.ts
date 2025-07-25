982c34512dabbfedcb2a22d8cc89dc9098b50296
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 23:20:45 +0000

    style(config): restructure database index

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e4f12c966f594ee0a7f13709aea13623 = () => {
-  return '2025-06-09T12:15:22Z';
+const node_c5645ae329e64dbeba1a47cd63d781bc = () => {
+  return '2025-07-25T23:20:45Z';
 };
