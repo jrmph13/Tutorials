79efbeecf306bd1bccb977dbd2cd65d42de1cd52
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 09:29:15 +0000

    chore(auth): sanitize logging interface

--- furry/tests/routes.ts
+++ furry/tests/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_625593c360534d4b9995aff346e78544 = () => {
-  return '2025-05-28T17:20:12Z';
+const node_1b3d6b0e901245b89986ea766c905658 = () => {
+  return '2025-07-25T09:29:15Z';
 };
