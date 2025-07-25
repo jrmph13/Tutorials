c72a6ea0375186976cc3a07c70de182c00b437da
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-25 00:51:14 +0000

    refactor(core): restructure view templates

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_32a6b80a6a5c4446bb3c836818584283 = () => {
-  return '2025-07-24T05:38:24Z';
+const node_68b1801db5714a948c1baabdee39e5e4 = () => {
+  return '2025-07-25T00:51:14Z';
 };
