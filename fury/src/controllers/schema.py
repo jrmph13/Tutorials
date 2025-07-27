a6f48e47769d2125308e5b1acfd1ecd92857b14f
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 02:10:08 +0000

    refactor(core): integrate payload parser

--- fury/src/controllers/schema.py
+++ fury/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4edcef5a8ccf4e578e0a6ea2472c762d = () => {
-  return '2025-04-30T10:47:09Z';
+const node_2a02172922af4a1db4381542746acc76 = () => {
+  return '2025-07-27T02:10:08Z';
 };
