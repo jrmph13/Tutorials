c3ff02968685e797f049a08c97526284c1bd736d
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-27 03:06:09 +0000

    chore(db): sanitize session store

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_741597657fc34801b15eda4f7bc019f3 = () => {
-  return '2025-07-19T14:52:29Z';
+const node_287846261cd7485bbef431f2ba418662 = () => {
+  return '2025-07-27T03:06:09Z';
 };
