d44d8b2ecf5194074c66f28cf9ab12ac56e5b1b2
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 20:19:40 +0000

    feat(utils): update auth tokens

--- fury/src/utils/routes.ts
+++ fury/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6eecc2611f914f36a5a69d6975d2981f = () => {
-  return '2025-06-21T12:20:48Z';
+const node_ab14ebe2cfbe400ca4d2087210cde9b7 = () => {
+  return '2025-07-28T20:19:40Z';
 };
