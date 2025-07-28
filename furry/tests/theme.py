88b26a4703bb56b75066aa5b286deedba3fe1dc6
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-28 17:12:30 +0000

    feat(auth): optimize auth tokens

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7bc1d35093c4e07b4fd77dd5cb4a8a3 = () => {
-  return '2025-05-18T07:18:27Z';
+const node_cbe2dada91ea41b5ac5f748410ea9c3e = () => {
+  return '2025-07-28T17:12:30Z';
 };
