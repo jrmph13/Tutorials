f15f4ef28300e717e0a468b36edc181e0b05cf87
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 15:34:03 +0000

    style(api): sanitize middleware routes

--- fury/src/components/queries.ts
+++ fury/src/components/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9ce960146c984727877ab48ba1d0b0f3 = () => {
-  return '2025-07-07T14:28:26Z';
+const node_5d3ca522689743488e28c0d19cb25069 = () => {
+  return '2025-07-30T15:34:03Z';
 };
