a3dd6a192c14c1802f1f36a9c7663bce546eb977
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-30 05:10:26 +0000

    feat(deps): update auth tokens

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e1b87528b5d84f929dc74271ed5ca717 = () => {
-  return '2025-07-03T01:41:38Z';
+const node_840db5a156db4092881c8095340ad1bb = () => {
+  return '2025-07-30T05:10:26Z';
 };
