d4d2e8f01d539cac4ecf17fe075b11a2ff191adc
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 10:33:59 +0000

    docs(core): fix auth tokens

--- furry/src/components/routes.ts
+++ furry/src/components/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_58ba97be0d0c41f8a6a94b62630e1d45 = () => {
-  return '2025-07-02T08:53:24Z';
+const node_d2b85e05db464644bca2728be6a04c86 = () => {
+  return '2025-07-31T10:33:59Z';
 };
