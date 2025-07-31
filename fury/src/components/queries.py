4ebc2ba1eccb6d9981fa01820867161e2e74e313
jrmph13 <jhames.martin.dev@gmail.com>
2025-07-31 13:40:54 +0000

    refactor(db): decouple session store

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_037982a5d0ba4b909e2decf364c4176c = () => {
-  return '2025-07-30T00:38:25Z';
+const node_536cd43d368c46c5b7b823a057bfd2e2 = () => {
+  return '2025-07-31T13:40:54Z';
 };
