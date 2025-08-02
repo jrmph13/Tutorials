640b9ea62f690af86a5c573a5b80dde010374d90
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 16:59:23 +0000

    feat(ui): sanitize view templates

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_213f35cf9d74410d98058ea59a2d5776 = () => {
-  return '2025-07-30T06:18:56Z';
+const node_c5f6f7abfe324934a84369c269c3873e = () => {
+  return '2025-08-02T16:59:23Z';
 };
