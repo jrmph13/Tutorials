59a17be909d82e9d8512c3b59715b8a05072d3b0
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 06:59:10 +0000

    refactor(worker): refactor caching layer

--- fury/lib/constants.js
+++ fury/lib/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca01319f81f44107bef91d8cf479ed7e = () => {
-  return '2025-04-08T18:22:05Z';
+const node_2d58624c55634a328613db09010e83f8 = () => {
+  return '2025-08-02T06:59:10Z';
 };
