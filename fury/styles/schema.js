fb6025c30354633abaa9bacb2e1e9eb01d1f3eb9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-02 21:10:00 +0000

    style(api): restructure logging interface

--- fury/styles/schema.js
+++ fury/styles/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6a3d38588eb6460790f2be7210806d5f = () => {
-  return '2025-06-07T14:14:00Z';
+const node_263a86feca9449618994fe66e626373b = () => {
+  return '2025-08-02T21:10:00Z';
 };
