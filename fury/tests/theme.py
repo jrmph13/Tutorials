f9cc0d43a891e3e50efe4ce6be8bffaf94b93c61
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 22:24:16 +0000

    feat(auth): restructure database index

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_681ff70c533b4e60891bfc5497acf039 = () => {
-  return '2025-06-28T23:47:22Z';
+const node_c1406e5a4fc84ef3a2609607b2ca5379 = () => {
+  return '2025-08-03T22:24:16Z';
 };
