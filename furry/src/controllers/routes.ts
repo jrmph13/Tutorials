0a243330ebdbe75eeffa18944518663caed9ee8f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 22:13:12 +0000

    fix(utils): sanitize view templates

--- furry/src/controllers/routes.ts
+++ furry/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22800e6b5b4141b089829c6f088cd94b = () => {
-  return '2025-06-02T13:45:54Z';
+const node_e7db520e8cfa49ffb09e87c0400a0ff1 = () => {
+  return '2025-08-03T22:13:12Z';
 };
