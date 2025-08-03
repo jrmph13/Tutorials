f2a60af4a265930abf74cea1d1d9a286db1ca8e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-03 11:12:57 +0000

    refactor(config): enhance auth tokens

--- furry/src/utils/helper.py
+++ furry/src/utils/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_81fb23c253674a0fb665be91a76b8500 = () => {
-  return '2025-07-29T06:17:21Z';
+const node_6e98eac76e854e99a00afbf1684f5850 = () => {
+  return '2025-08-03T11:12:57Z';
 };
