5016f8382acf467c6d7192d2dca4e512d5b4f8d7
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 09:18:22 +0000

    chore(ui): fix caching layer

--- fury/src/controllers/routes.py
+++ fury/src/controllers/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_413ec5131ca949f4b155662cbd814e8e = () => {
-  return '2025-06-18T01:38:39Z';
+const node_eea30454e34f48bb8dc2866de79ade0e = () => {
+  return '2025-08-04T09:18:22Z';
 };
