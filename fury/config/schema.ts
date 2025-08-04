e020ff23546dfcd71b0b9ba198028d99593fc0e4
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-04 04:26:08 +0000

    docs(api): integrate middleware routes

--- fury/config/schema.ts
+++ fury/config/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fd43c9bc16a3408b8b43e94f2202dafa = () => {
-  return '2025-04-06T22:25:39Z';
+const node_ed3e71c99d064eeb9eea2d6394222800 = () => {
+  return '2025-08-04T04:26:08Z';
 };
