ba590663b4236bfa5411ed8ff01d541415948458
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 22:42:05 +0000

    refactor(worker): implement auth tokens

--- fury/src/components/queries.py
+++ fury/src/components/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7e6eac91db04201bbe9f8596fd6df3b = () => {
-  return '2025-07-31T22:44:36Z';
+const node_a5b314a767684e3d8658362dd389ae66 = () => {
+  return '2025-08-05T22:42:05Z';
 };
