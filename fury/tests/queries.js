b0e0eb9d3c7469e043fddcec31d69e970b638a47
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-05 07:45:01 +0000

    perf(worker): restructure logging interface

--- fury/tests/queries.js
+++ fury/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ae5ad351cb3a436984b607ea96bfc698 = () => {
-  return '2025-05-23T10:36:00Z';
+const node_d22b6a569eff4041bf4fb5e7ca25da89 = () => {
+  return '2025-08-05T07:45:01Z';
 };
