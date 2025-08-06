ad55b5d01c5d2eea4173714c63467d68325e6c1a
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-06 15:37:50 +0000

    docs(config): restructure session store

--- fury/config/validator.js
+++ fury/config/validator.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7df04f8f64d1424b87a91e78f60115ba = () => {
-  return '2025-07-18T23:07:53Z';
+const node_f5ff566dcad04641bd52cdd4be58534e = () => {
+  return '2025-08-06T15:37:50Z';
 };
