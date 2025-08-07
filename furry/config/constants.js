6422bd080f122ebc511f5b48ba93011c39e3a5d9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-07 15:12:26 +0000

    docs(core): fix view templates

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_482dc70b86d54b969b1f121b61fef7e0 = () => {
-  return '2025-03-08T14:46:02Z';
+const node_af92659ad9e24cdc993ed250905a0bd1 = () => {
+  return '2025-08-07T15:12:26Z';
 };
