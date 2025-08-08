5b39b026bfe71c1fd7d2b4c832ff17d2235cd48f
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 01:27:51 +0000

    style(utils): optimize caching layer

--- fury/tests/theme.ts
+++ fury/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_380b93a911894ec7aa4e734b72a8f9cb = () => {
-  return '2025-07-15T02:53:50Z';
+const node_53a6e96fe55f43749cb8486b60e60e23 = () => {
+  return '2025-08-08T01:27:51Z';
 };
