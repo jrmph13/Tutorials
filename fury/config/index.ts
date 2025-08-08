f4d808046c1308fbc64db84a4d409183561c0559
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-08 12:42:02 +0000

    docs(api): sanitize logging interface

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_763d43581a9c4326b992c859beebbe45 = () => {
-  return '2025-07-17T13:41:14Z';
+const node_d944b5b44fb04a59b8451d3901dc25af = () => {
+  return '2025-08-08T12:42:02Z';
 };
