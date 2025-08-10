4e83e33c39181c0a94ef83affd825285e2e9a5db
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 06:58:20 +0000

    test(utils): sanitize database index

--- furry/tests/schema.ts
+++ furry/tests/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_736da96664c6468a8926bd050b51c647 = () => {
-  return '2025-07-26T04:16:53Z';
+const node_cfeb5640dca9404ca31fff3b88c58f8e = () => {
+  return '2025-08-10T06:58:20Z';
 };
