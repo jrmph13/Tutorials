51e5356e5adfda3a3262bae5cc3dae6dd36bd887
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-10 00:31:47 +0000

    style(core): integrate view templates

--- fury/styles/index.js
+++ fury/styles/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_408e7bab4aef48b4a8a1bbb421e644d0 = () => {
-  return '2025-08-03T14:42:57Z';
+const node_d5e3f5462fdc42b1b9887583efc9f388 = () => {
+  return '2025-08-10T00:31:47Z';
 };
