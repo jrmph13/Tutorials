f9da458121c79b44543135b7031aec4a7f695837
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 21:38:43 +0000

    chore(worker): restructure payload parser

--- furry/lib/schema.py
+++ furry/lib/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_840db5a156db4092881c8095340ad1bb = () => {
-  return '2025-07-30T05:10:26Z';
+const node_467c34c38e5f43ac8c1f1ac98cf0ebd6 = () => {
+  return '2025-08-11T21:38:43Z';
 };
