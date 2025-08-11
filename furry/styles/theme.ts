4db8fdbd34c0debf55c9afe1c34e5d1f1efcbc61
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-11 09:37:59 +0000

    chore(deps): restructure view templates

--- furry/styles/theme.ts
+++ furry/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb260c926b534971b3b123bfdb9b70eb = () => {
-  return '2025-07-26T18:37:34Z';
+const node_7785e7eb20c3425ab90c411a241ce467 = () => {
+  return '2025-08-11T09:37:59Z';
 };
