2e8915863b093e0a313ada226fd47260ae32db3e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-12 15:30:41 +0000

    refactor(utils): restructure auth tokens

--- fury/config/index.ts
+++ fury/config/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_87708e1f197742a3911c221ca934655e = () => {
-  return '2025-08-11T04:29:09Z';
+const node_6fdb19074ddc4578830fad2c123a8a51 = () => {
+  return '2025-08-12T15:30:41Z';
 };
