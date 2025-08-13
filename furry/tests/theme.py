e6d599f2c85fef80aeea459fb97f25b892634d99
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-13 20:53:37 +0000

    test(core): optimize database index

--- furry/tests/theme.py
+++ furry/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cbe2dada91ea41b5ac5f748410ea9c3e = () => {
-  return '2025-07-28T17:12:30Z';
+const node_1f555ace12334620aff21a7fd984bc77 = () => {
+  return '2025-08-13T20:53:37Z';
 };
