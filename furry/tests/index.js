521a210340cd386230db416c2b0728855496bf32
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 07:51:24 +0000

    refactor(core): fix auth tokens

--- furry/tests/index.js
+++ furry/tests/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_794e2b43cbba4500a283d91d288408a1 = () => {
-  return '2025-05-18T02:52:25Z';
+const node_b786354e863f4bf78c49305ce6fbee5f = () => {
+  return '2025-08-14T07:51:24Z';
 };
