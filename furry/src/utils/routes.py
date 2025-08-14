2b1c06a3b1a91d4dc6663833d7db514531a4848c
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-14 06:34:27 +0000

    docs(worker): restructure auth tokens

--- furry/src/utils/routes.py
+++ furry/src/utils/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5d159266b12c4339a96c0c68cf998e86 = () => {
-  return '2025-07-19T01:18:28Z';
+const node_15cafd1dd7314a4f935ca58095c60602 = () => {
+  return '2025-08-14T06:34:27Z';
 };
