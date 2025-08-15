cfea63999507e6ffe31ee3c50591079f8baf15d1
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-15 21:01:55 +0000

    docs(core): integrate auth tokens

--- fury/src/utils/helper.js
+++ fury/src/utils/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_94f9cd32c72248808a2a0affd1fceaa8 = () => {
-  return '2025-08-07T07:08:10Z';
+const node_b6f211202d604972a27794a4aa9f4ffd = () => {
+  return '2025-08-15T21:01:55Z';
 };
