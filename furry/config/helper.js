9156fc9ab073efab55494802fe59838ed9a7b559
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-18 19:07:17 +0000

    chore(config): optimize session store

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_fde2ce0b62df449eb4263816c89f92aa = () => {
-  return '2025-07-20T15:18:28Z';
+const node_efc8220ab6b5413b8eb1ce1cbd78dba3 = () => {
+  return '2025-08-18T19:07:17Z';
 };
