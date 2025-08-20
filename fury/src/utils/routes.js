0d775271020ae4a294cfc85606abe23c05837bf9
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-20 10:01:05 +0000

    perf(utils): restructure database index

--- fury/src/utils/routes.js
+++ fury/src/utils/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a7042854fb4143ab937fd0d76e1313f6 = () => {
-  return '2025-08-11T18:02:21Z';
+const node_0a7a2e6ee83842e7b43760d78a240283 = () => {
+  return '2025-08-20T10:01:05Z';
 };
