085afcbe0a2318ef6aeaee52a813bca4ddff2d5e
jrmph13 <jhames.martin.dev@gmail.com>
2025-08-26 02:40:36 +0000

    feat(db): sanitize database index

--- fury/src/utils/schema.py
+++ fury/src/utils/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6dff52e1803a4bc1a8e72fc561738f8b = () => {
-  return '2025-07-31T07:33:11Z';
+const node_a01b61a2985849a08d1e0ae288f0a172 = () => {
+  return '2025-08-26T02:40:36Z';
 };
