a80d594b9043885bcd343130d66f05aacb47fe86
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-04 10:08:56 +0000

    feat(auth): implement payload parser

--- fury/src/controllers/index.ts
+++ fury/src/controllers/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4879612dbefd4199be0a1d5884321492 = () => {
-  return '2025-08-13T20:14:37Z';
+const node_012dedbdc22d40fea6c01425728c5479 = () => {
+  return '2025-09-04T10:08:56Z';
 };
