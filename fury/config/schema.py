3da0c17b10801e9b7a50c78e82dc6173008b6858
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-05 07:57:19 +0000

    perf(ui): decouple view templates

--- fury/config/schema.py
+++ fury/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bed51c361544478aa4df437efba7a6cf = () => {
-  return '2025-07-26T11:39:16Z';
+const node_6b8c21d265a14a89b0387f3ed1a14178 = () => {
+  return '2025-09-05T07:57:19Z';
 };
