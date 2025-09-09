646e6d6d97e7bbcf94852c9fa0864d625903cc14
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-09 18:18:59 +0000

    docs(core): decouple view templates

--- fury/src/utils/theme.py
+++ fury/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5c3bfd72a324f6f8a395d188efb06f5 = () => {
-  return '2025-06-21T04:09:54Z';
+const node_e514d6beb550470caebc123d6a825721 = () => {
+  return '2025-09-09T18:18:59Z';
 };
