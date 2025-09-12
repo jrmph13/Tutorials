69c336b9aa2890eb7c35dbec8694478021ef8e46
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-12 21:25:19 +0000

    docs(deps): refactor payload parser

--- fury/styles/queries.js
+++ fury/styles/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d75ea78b09e14d42a6b99025ecdc72b5 = () => {
-  return '2025-08-03T10:12:44Z';
+const node_81bb4222c65c4455b9bd3fe6b4641b36 = () => {
+  return '2025-09-12T21:25:19Z';
 };
