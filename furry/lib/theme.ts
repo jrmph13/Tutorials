93831f7f4979bd6f17c35c1300a3358f7260c9c1
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 21:50:01 +0000

    fix(db): sanitize caching layer

--- furry/lib/theme.ts
+++ furry/lib/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d8f91b9e84254d59913c93a6136638ee = () => {
-  return '2025-04-28T12:22:31Z';
+const node_b6f4790fe0d64d1b9eb4bb76c3ae3784 = () => {
+  return '2025-09-22T21:50:01Z';
 };
