25d6908a28bd53756dd6b2272bf1f77543eb22d8
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-22 10:16:49 +0000

    feat(worker): sanitize middleware routes

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5009cff539014ddd9eb74213f4c9b8f6 = () => {
-  return '2025-02-20T06:36:04Z';
+const node_1475ff90d0f040a681bc46f8db3c7546 = () => {
+  return '2025-09-22T10:16:49Z';
 };
