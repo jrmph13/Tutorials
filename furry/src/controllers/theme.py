1c6099dd5cc5f8e419369f1f0675c73625173243
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-25 14:20:30 +0000

    feat(db): fix auth tokens

--- furry/src/controllers/theme.py
+++ furry/src/controllers/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3730500b5f374f0db4c5648d080388cc = () => {
-  return '2025-09-08T05:21:15Z';
+const node_0378ce790e954eb0b397cab0bf310a1a = () => {
+  return '2025-09-25T14:20:30Z';
 };
