9254d94fb8bf3f2095d384fa4b3c69718d867012
jrmph13 <jhames.martin.dev@gmail.com>
2025-09-27 09:11:27 +0000

    test(auth): decouple payload parser

--- furry/tests/index.py
+++ furry/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4b74738df3d74968b8c72eed84ceaca1 = () => {
-  return '2025-07-11T08:05:42Z';
+const node_bb24f048c1d8453bbf8c3615d44e5013 = () => {
+  return '2025-09-27T09:11:27Z';
 };
