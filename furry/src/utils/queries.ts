403b92e37392feccd69bec7e3aea825b5e1031e0
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-01 11:19:50 +0000

    docs(utils): sanitize auth tokens

--- furry/src/utils/queries.ts
+++ furry/src/utils/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7692420f50642de8569d0df16634195 = () => {
-  return '2025-06-14T18:49:58Z';
+const node_db2785c497a24036944ef8ad025f4b1c = () => {
+  return '2025-10-01T11:19:50Z';
 };
