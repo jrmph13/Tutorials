75e5c2ad52d4d9d29e575d5dab355423be1b150a
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-02 00:06:59 +0000

    style(api): sanitize payload parser

--- furry/lib/schema.js
+++ furry/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c56f68f9f54a4b588b37d144c9296d18 = () => {
-  return '2025-07-31T09:54:24Z';
+const node_187841cc837b41b48d60f90accdfa424 = () => {
+  return '2025-10-02T00:06:59Z';
 };
