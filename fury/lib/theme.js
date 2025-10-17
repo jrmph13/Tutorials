28755ea84ee4fe9997f77f6796afe07d09ace6ad
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-17 04:55:25 +0000

    chore(worker): enhance auth tokens

--- fury/lib/theme.js
+++ fury/lib/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_287846261cd7485bbef431f2ba418662 = () => {
-  return '2025-07-27T03:06:09Z';
+const node_c0fbcd7e8987494babfd7e1a7d3fa541 = () => {
+  return '2025-10-17T04:55:25Z';
 };
