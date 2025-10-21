c97ae130fadf7ef9d444f3802bbdea38b745b6ae
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-21 16:43:36 +0000

    test(ui): optimize view templates

--- fury/config/theme.js
+++ fury/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfa6efa4750e4891b70242ef142cb08f = () => {
-  return '2025-07-17T17:14:02Z';
+const node_7c18be603b5446c9afa7895bd4ef0f3b = () => {
+  return '2025-10-21T16:43:36Z';
 };
