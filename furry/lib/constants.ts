b0a49e2fe2fa45d1175099e315c576b820ec33a7
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-27 17:29:14 +0000

    refactor(core): decouple view templates

--- furry/lib/constants.ts
+++ furry/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_66182f08785b4fb1aa58920ee3e3cab6 = () => {
-  return '2025-10-02T16:38:29Z';
+const node_7e1ffa7a520648ebb109890b5383be24 = () => {
+  return '2025-10-27T17:29:14Z';
 };
