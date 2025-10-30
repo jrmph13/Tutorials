79fbbba050115a669f5fc1cd2b48f96d184b1b4c
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 16:44:04 +0000

    chore(utils): optimize payload parser

--- fury/src/components/index.js
+++ fury/src/components/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cb680d3ed6a94976aead89f58d23fc7d = () => {
-  return '2025-08-14T21:58:46Z';
+const node_62cf0b7de9514e6bb678e3027da0f13c = () => {
+  return '2025-10-30T16:44:04Z';
 };
