c85d0a35ebfe963e5e4dde4c0c3ac5f9912e6dc9
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 02:34:53 +0000

    refactor(auth): integrate logging interface

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c5f6f7abfe324934a84369c269c3873e = () => {
-  return '2025-08-02T16:59:23Z';
+const node_ecb83d2af4c347468ec85c102b1f95aa = () => {
+  return '2025-10-30T02:34:53Z';
 };
