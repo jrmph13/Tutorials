b77c3f41387a640439933b80f5b228a566a411c0
jrmph13 <jhames.martin.dev@gmail.com>
2025-10-30 13:16:30 +0000

    test(core): decouple logging interface

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb9a6a691d7c425f988dabb8fb9d8676 = () => {
-  return '2025-10-26T22:37:52Z';
+const node_83395189b16342c1a7e245354a86061d = () => {
+  return '2025-10-30T13:16:30Z';
 };
