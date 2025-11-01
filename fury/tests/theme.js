27448933c76cd9a6fa29942c312a3eb83d8cefc1
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-01 02:50:56 +0000

    fix(core): restructure session store

--- fury/tests/theme.js
+++ fury/tests/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1b1dbb8ee0094b74bf83d48f241d9a30 = () => {
-  return '2025-05-10T23:23:18Z';
+const node_5d88b7e62b4042878b8dae2f918637c7 = () => {
+  return '2025-11-01T02:50:56Z';
 };
