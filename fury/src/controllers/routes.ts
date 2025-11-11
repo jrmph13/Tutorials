88efaf42dfb41cf572d5e8c977293bb380901f28
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 20:53:07 +0000

    perf(auth): sanitize validation pipeline

--- fury/src/controllers/routes.ts
+++ fury/src/controllers/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_453fe634d0ad48fb84d5222fcbf8fd4a = () => {
-  return '2025-05-15T01:56:20Z';
+const node_ee9b39f2882f4493816bd5e1973a0796 = () => {
+  return '2025-11-11T20:53:07Z';
 };
