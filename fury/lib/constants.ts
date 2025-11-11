27c7abbc2ddb3f2eef7807e1d56fc34882d19477
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 10:51:44 +0000

    test(worker): refactor logging interface

--- fury/lib/constants.ts
+++ fury/lib/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_694ce9ded4554c88bb802bbe2795e6b2 = () => {
-  return '2025-05-17T07:40:30Z';
+const node_ec519754ac4a48f3b3fd4f2e8cec8a7b = () => {
+  return '2025-11-11T10:51:44Z';
 };
