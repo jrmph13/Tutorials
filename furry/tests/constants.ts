dd1837b0a9e0d549953d110d37051443cfb16ba5
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-11 02:20:47 +0000

    test(worker): restructure caching layer

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_08af78a5ff394527ac0f9fd9fc9d8a95 = () => {
-  return '2025-09-12T18:20:24Z';
+const node_1a401af2a55f49c18200aa6c811d8da8 = () => {
+  return '2025-11-11T02:20:47Z';
 };
