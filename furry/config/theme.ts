1d2989fb95a8838212a644a3ff7b481249bc11c4
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-13 10:41:55 +0000

    fix(config): optimize auth tokens

--- furry/config/theme.ts
+++ furry/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c7fd3e8e880e4ee6b3730fd2ff6583e6 = () => {
-  return '2025-08-04T23:58:53Z';
+const node_337205555e34486cba9baca8c2a14592 = () => {
+  return '2025-11-13T10:41:55Z';
 };
