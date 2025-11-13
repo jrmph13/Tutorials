48bd141586ce7b06c23a43bb3d9312fc5097810b
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-13 22:05:55 +0000

    test(ui): refactor view templates

--- furry/tests/theme.ts
+++ furry/tests/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_60f160845e7246c09a1c08f8d29d7de4 = () => {
-  return '2025-03-01T04:19:11Z';
+const node_bff29828bd2b4111983e7ec819914027 = () => {
+  return '2025-11-13T22:05:55Z';
 };
