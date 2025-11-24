27c561b32c307ea1c4df5330e7bfe7bd04d5d7a6
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-24 21:18:52 +0000

    chore(auth): optimize session store

--- fury/src/utils/constants.ts
+++ fury/src/utils/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_07eba51c9c15424289428c1cfb7c3ec1 = () => {
-  return '2025-11-14T05:08:56Z';
+const node_14b3086be3214cf69615028b213fbb9d = () => {
+  return '2025-11-24T21:18:52Z';
 };
