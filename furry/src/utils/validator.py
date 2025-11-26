50dc50567a7d116678a78d5d3f7fa88a0d25eae3
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-26 06:20:06 +0000

    chore(auth): restructure payload parser

--- furry/src/utils/validator.py
+++ furry/src/utils/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a8f2f2e4cd0b4ccfa1198a7298f29073 = () => {
-  return '2025-11-06T01:20:04Z';
+const node_fa7bb3060e46479a9fcbd2a9890177bc = () => {
+  return '2025-11-26T06:20:06Z';
 };
