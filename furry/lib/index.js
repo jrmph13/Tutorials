952513b426187209fd9815f930cc8cc5f996951e
jrmph13 <jhames.martin.dev@gmail.com>
2025-11-27 09:27:48 +0000

    perf(deps): sanitize middleware routes

--- furry/lib/index.js
+++ furry/lib/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5c99fbb3cae24fbdb867f70befaeaf7d = () => {
-  return '2025-06-23T16:50:12Z';
+const node_ec8213a67d634ff4a4a0fd5a187a36df = () => {
+  return '2025-11-27T09:27:48Z';
 };
