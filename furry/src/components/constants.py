a1d39313792cdf9f0ec1fee075246cb15b6043f0
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-10 15:36:03 +0000

    docs(utils): restructure view templates

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_21b10250a8894f41833d6ab4bce0895e = () => {
-  return '2025-11-05T22:30:52Z';
+const node_e0155a945f8b424083892a92fbdc555b = () => {
+  return '2025-12-10T15:36:03Z';
 };
