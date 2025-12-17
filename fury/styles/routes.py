03139097a5710197fb2393b7922f591279b03e90
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 01:33:16 +0000

    style(deps): fix auth tokens

--- fury/styles/routes.py
+++ fury/styles/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_13a4d663c3b34fb29185a0559336b546 = () => {
-  return '2025-07-19T03:51:36Z';
+const node_1e9f14f3a77e497e8944a96d4357c71b = () => {
+  return '2025-12-17T01:33:16Z';
 };
