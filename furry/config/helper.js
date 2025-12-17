1a9bc668a0a349bbc4060f184d27b7ba4ac4b840
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-17 01:09:59 +0000

    chore(worker): update database index

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e5928964b8304a57a233b8f9d7e7a54b = () => {
-  return '2025-08-29T23:46:03Z';
+const node_4fda71ff77ae49268a488ef2f38ca2df = () => {
+  return '2025-12-17T01:09:59Z';
 };
