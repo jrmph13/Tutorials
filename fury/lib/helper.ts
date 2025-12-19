5bf521e46f40cf8a4947a32285b4ba4be83ce479
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-19 07:33:23 +0000

    style(utils): decouple view templates

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d587dd18eee64e73b7240123ac50a4c9 = () => {
-  return '2025-11-26T23:35:44Z';
+const node_24253a48af1e478da90f245fc8eba420 = () => {
+  return '2025-12-19T07:33:23Z';
 };
