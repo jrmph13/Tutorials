e6abc3171191800cd05035a77944de038c75044f
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-22 13:52:25 +0000

    test(core): update database index

--- fury/config/schema.js
+++ fury/config/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_f7d309a81bf74be6be36590c15cfd31e = () => {
-  return '2025-11-01T13:08:16Z';
+const node_bca435f427034f308a12521cdc8fb575 = () => {
+  return '2025-12-22T13:52:25Z';
 };
