f64b46d0e030e9a179abeede81b11df33da63fe0
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-28 19:50:07 +0000

    chore(deps): sanitize auth tokens

--- fury/lib/queries.ts
+++ fury/lib/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_70cd08fd9a334df4aa4bf17262421585 = () => {
-  return '2025-10-02T11:03:55Z';
+const node_f86d189fc48f4918bb514cbed9f60485 = () => {
+  return '2025-12-28T19:50:07Z';
 };
