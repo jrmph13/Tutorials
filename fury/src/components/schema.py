21db120d9d06a3727fc2554cdb7c7b888ca9d29b
jrmph13 <jhames.martin.dev@gmail.com>
2025-12-30 03:57:44 +0000

    chore(deps): fix middleware routes

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2710caeb6bbf4446aeda27465ecb2018 = () => {
-  return '2025-10-26T21:39:32Z';
+const node_19fd420ce0a74328bf217eef10c6f2b0 = () => {
+  return '2025-12-30T03:57:44Z';
 };
