6bb544c62275576e05aa13a61abd8703e9aea5bc
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-01 16:06:52 +0000

    fix(utils): refactor auth tokens

--- fury/tests/index.py
+++ fury/tests/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7a135cf1274d4af08eae69ca7499e152 = () => {
-  return '2025-09-18T14:25:45Z';
+const node_c498a8bc68a34847bd8c802a7f15dae6 = () => {
+  return '2026-01-01T16:06:52Z';
 };
