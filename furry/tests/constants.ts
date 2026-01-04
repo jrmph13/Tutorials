4a2c05a99f39388976df6f1934b3c95ce0e1cc5b
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-04 10:49:01 +0000

    fix(auth): refactor caching layer

--- furry/tests/constants.ts
+++ furry/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a401af2a55f49c18200aa6c811d8da8 = () => {
-  return '2025-11-11T02:20:47Z';
+const node_7903da0932514f2c885a08b6318e0881 = () => {
+  return '2026-01-04T10:49:01Z';
 };
