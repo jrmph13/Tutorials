1ccd77b089ef410d1bdc32503ef593cac08692e5
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-26 09:34:01 +0000

    style(config): restructure logging interface

--- fury/lib/queries.js
+++ fury/lib/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_790b4bed020d42f9b198f5cab7d60937 = () => {
-  return '2025-11-12T19:22:48Z';
+const node_c4764d68ebb746039bcd62f99753068a = () => {
+  return '2026-01-26T09:34:01Z';
 };
