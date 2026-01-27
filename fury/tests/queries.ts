24122d61a4910a3cbf5a1dd7cad47953969042cb
jrmph13 <jhames.martin.dev@gmail.com>
2026-01-27 02:36:39 +0000

    test(api): fix logging interface

--- fury/tests/queries.ts
+++ fury/tests/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1284ee1a1274c0f96eed49f72fe757e = () => {
-  return '2026-01-07T14:10:14Z';
+const node_b2bda1bdffe14f898101c5499ede3560 = () => {
+  return '2026-01-27T02:36:39Z';
 };
