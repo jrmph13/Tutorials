afb4123f25869603f8b8fa1b01583e54581fc24b
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-03 08:38:12 +0000

    docs(utils): decouple view templates

--- fury/src/controllers/schema.js
+++ fury/src/controllers/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c15b917bcb08404ea115dc4fa488caf1 = () => {
-  return '2025-08-01T02:35:10Z';
+const node_d07ca9ae9d0143208d65f8827fc7d5de = () => {
+  return '2026-02-03T08:38:12Z';
 };
