1a49374a26cbe178aa93b2ea8bd23286be564266
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-04 07:28:32 +0000

    fix(worker): integrate payload parser

--- furry/src/utils/index.js
+++ furry/src/utils/index.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_c3dd0f3b322c4f989dde892e40748e28 = () => {
-  return '2025-11-26T07:41:33Z';
+const node_397dfde038e646bab9b1e150ee02c2e4 = () => {
+  return '2026-02-04T07:28:32Z';
 };
