2be31385d320a11f35ecb0e878bcde692b5314db
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-05 21:50:03 +0000

    feat(deps): update payload parser

--- fury/src/controllers/constants.ts
+++ fury/src/controllers/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6df32864d6f34aa8a484919d19182180 = () => {
-  return '2026-02-04T17:57:50Z';
+const node_0d14f31da79d4b39b68208be7e6fff8c = () => {
+  return '2026-02-05T21:50:03Z';
 };
