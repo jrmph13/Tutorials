e0c4a636f0ee2b9c007c2def90513f0b4d49e396
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-09 11:30:24 +0000

    perf(core): optimize payload parser

--- fury/src/components/helper.py
+++ fury/src/components/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9a924df4c6114f92a3a98fc0a5e61b7b = () => {
-  return '2025-11-26T10:00:25Z';
+const node_cbb5e4891a014cf7a777c7a5d3224d9f = () => {
+  return '2026-02-09T11:30:24Z';
 };
