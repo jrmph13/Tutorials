e6ddb2a4845ea617b5c3c3d7f86610eb18e066af
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-14 16:39:51 +0000

    fix(api): update caching layer

--- fury/config/queries.ts
+++ fury/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a93db3b7a4bc45f2a6be25867b585a28 = () => {
-  return '2026-02-02T20:37:06Z';
+const node_dd31705c62914c929d19266013a9565b = () => {
+  return '2026-02-14T16:39:51Z';
 };
