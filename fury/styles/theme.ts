cadba6576e9cd1f8a6a3f41e11c39b7f3dff377d
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-17 11:55:34 +0000

    perf(config): fix database index

--- fury/styles/theme.ts
+++ fury/styles/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5536871c5d1d4566b71433a22039ca45 = () => {
-  return '2025-12-11T00:01:03Z';
+const node_0d959b570ccb467facfb6eb7c0f347da = () => {
+  return '2026-02-17T11:55:34Z';
 };
