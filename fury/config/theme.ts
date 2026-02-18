daa2c3d24d66f559b8627fa131ea3dc42b4ef3cb
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-18 02:20:32 +0000

    fix(config): sanitize payload parser

--- fury/config/theme.ts
+++ fury/config/theme.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_6e9bade384fb45cc96758575ff8bcd8c = () => {
-  return '2025-08-11T01:27:15Z';
+const node_e0eb7189011c4a68b290731e06678321 = () => {
+  return '2026-02-18T02:20:32Z';
 };
