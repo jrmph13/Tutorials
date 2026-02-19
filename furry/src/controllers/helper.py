eb1b7948e05a323fb2596e100b1747f8480c06d2
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-19 01:21:02 +0000

    fix(auth): restructure middleware routes

--- furry/src/controllers/helper.py
+++ furry/src/controllers/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_9783d9a9ad0749ffad243151c05f1163 = () => {
-  return '2026-01-29T11:55:34Z';
+const node_dbde4001d39f41c688a1af5b182c544c = () => {
+  return '2026-02-19T01:21:02Z';
 };
