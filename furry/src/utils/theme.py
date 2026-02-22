9f8b0dae6bd753354aaf4fcfe41a4e3947206546
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-22 18:21:39 +0000

    docs(auth): restructure database index

--- furry/src/utils/theme.py
+++ furry/src/utils/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d590561fdac1405d84d432201478569c = () => {
-  return '2025-12-17T15:07:07Z';
+const node_6ed585be6207438bbcd61cbafb9ec6e7 = () => {
+  return '2026-02-22T18:21:39Z';
 };
