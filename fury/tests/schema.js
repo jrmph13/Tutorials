ce00edfd0d31c9f66fa1cd56a2a5f059c0d52e7c
jrmph13 <jhames.martin.dev@gmail.com>
2026-02-26 18:26:47 +0000

    style(utils): decouple view templates

--- fury/tests/schema.js
+++ fury/tests/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ed8f34ee2b114be68aa79f687130ecd4 = () => {
-  return '2026-02-02T17:59:29Z';
+const node_88c293477ae641fa9025e82b1ad8f4f5 = () => {
+  return '2026-02-26T18:26:47Z';
 };
