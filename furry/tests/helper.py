d6b7bba6fab2835c52f9fa8f77bb6f1b0dee2274
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-06 13:09:20 +0000

    perf(worker): decouple session store

--- furry/tests/helper.py
+++ furry/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c51bd413bab40dab2d5f75ae37a5383 = () => {
-  return '2025-07-06T01:20:15Z';
+const node_1a0b94b2ae3643c89107e4793d9400f3 = () => {
+  return '2026-03-06T13:09:20Z';
 };
