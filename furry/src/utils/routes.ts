7059e6631ac022bec52d888a48ba132e6a50a147
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 21:03:03 +0000

    chore(auth): update view templates

--- furry/src/utils/routes.ts
+++ furry/src/utils/routes.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d839ffd6e4ed41c9a4fa0532bac6835c = () => {
-  return '2026-03-06T12:19:43Z';
+const node_ee69fb8df65641deb85b38b81b535481 = () => {
+  return '2026-03-08T21:03:03Z';
 };
