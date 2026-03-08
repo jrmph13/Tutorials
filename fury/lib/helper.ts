a99df9a130256a852de79cb54bd2258d559af6b4
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-08 18:06:19 +0000

    fix(api): refactor caching layer

--- fury/lib/helper.ts
+++ fury/lib/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ca7979feb4c84e37828132d0e3baad14 = () => {
-  return '2026-02-20T08:21:10Z';
+const node_22943c41fce14fea89452f3c717e4514 = () => {
+  return '2026-03-08T18:06:19Z';
 };
