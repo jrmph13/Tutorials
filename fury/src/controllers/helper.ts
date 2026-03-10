1c6a669751570e5220c0b12417a6d61b508365d2
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-10 17:14:40 +0000

    docs(api): enhance view templates

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0e602c4f992a4079ac12731408bbbbb3 = () => {
-  return '2025-12-26T21:37:47Z';
+const node_ce28e5ee3a844dc484f1c3f1e55192b6 = () => {
+  return '2026-03-10T17:14:40Z';
 };
