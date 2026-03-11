7ff36b4a9d40ee9dce1d2df0babc2bea5aaf2521
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-11 08:06:40 +0000

    docs(config): restructure middleware routes

--- fury/lib/helper.py
+++ fury/lib/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1488064041284024a3d0c297263f384d = () => {
-  return '2025-07-14T16:24:40Z';
+const node_7656d287c28c42e9983157d487c717a8 = () => {
+  return '2026-03-11T08:06:40Z';
 };
