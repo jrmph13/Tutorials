7281fc6d9f1b03b013e6fd752185c575b2043e90
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 21:57:03 +0000

    feat(worker): fix payload parser

--- fury/styles/queries.ts
+++ fury/styles/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_eb0e97d7c4854f7192ab488d9eedd39a = () => {
-  return '2025-05-20T12:13:32Z';
+const node_0481d8c451fd457fbe1575433f3dcba3 = () => {
+  return '2026-03-13T21:57:03Z';
 };
