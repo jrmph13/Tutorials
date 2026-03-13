b3bc782ff0bb24ba8c7804730f32117b57f62d2f
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-13 20:00:51 +0000

    feat(ui): update payload parser

--- furry/tests/queries.js
+++ furry/tests/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_57b5a3faca4642d9915d26b98d2a74d3 = () => {
-  return '2025-11-11T06:12:10Z';
+const node_34440a90a729402189739aab2cc3fb1d = () => {
+  return '2026-03-13T20:00:51Z';
 };
