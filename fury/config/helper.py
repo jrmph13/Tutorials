1b181f333b6f296aa1d5fb2a4e002c5a54deabf2
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-15 16:35:43 +0000

    fix(api): integrate view templates

--- fury/config/helper.py
+++ fury/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ab7316d7d0354f10aa09394c9ca14830 = () => {
-  return '2025-07-28T04:06:02Z';
+const node_bd31cf1dfd2a4875b1e8c0b15c242a1a = () => {
+  return '2026-03-15T16:35:43Z';
 };
