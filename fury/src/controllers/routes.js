3c0df035d4685267d20158376000408d7c513892
jrmph13 <jhames.martin.dev@gmail.com>
2026-03-26 18:21:26 +0000

    chore(worker): enhance payload parser

--- fury/src/controllers/routes.js
+++ fury/src/controllers/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bb747f169dfb433fb7d8181c9ba69914 = () => {
-  return '2026-03-04T16:49:42Z';
+const node_17b31c65d0324b00b104d24afe537b2d = () => {
+  return '2026-03-26T18:21:26Z';
 };
