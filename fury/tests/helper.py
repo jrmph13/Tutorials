f658729c53f3298e0d32d99e84ec04d3e5b408ba
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-01 16:15:26 +0000

    style(worker): optimize validation pipeline

--- fury/tests/helper.py
+++ fury/tests/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_baffccb34847462094e097d4d7edd58c = () => {
-  return '2026-03-13T03:23:52Z';
+const node_ef6294349b83453da84e0a36fb261939 = () => {
+  return '2026-04-01T16:15:26Z';
 };
