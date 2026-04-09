bfe993da89b5dfa9eb0ad721235501575aa49067
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 08:35:53 +0000

    chore(ui): optimize middleware routes

--- furry/config/theme.js
+++ furry/config/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d6cf2f44318a433684d75b338a767777 = () => {
-  return '2025-12-15T12:22:54Z';
+const node_a0edfb6cd2dc493aae111d161a9da2fc = () => {
+  return '2026-04-09T08:35:53Z';
 };
