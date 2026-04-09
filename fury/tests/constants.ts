14abdafd229ee50d3bfa72c8a67f47cc3fb96804
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-09 01:15:53 +0000

    fix(auth): integrate view templates

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_2e9c084f15d54cef93e53b9111ed4396 = () => {
-  return '2025-11-04T21:16:04Z';
+const node_8c57fb222aa9440bbd9e38507dee61e3 = () => {
+  return '2026-04-09T01:15:53Z';
 };
