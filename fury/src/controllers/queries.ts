a297e5ad92d7ec577a2c84421fe124af4aa21858
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-11 00:22:48 +0000

    perf(core): fix auth tokens

--- fury/src/controllers/queries.ts
+++ fury/src/controllers/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aa6f4954bfed45babab773c595e8edb3 = () => {
-  return '2026-02-20T00:51:26Z';
+const node_14ba399b0c1b487d9da274aefd693866 = () => {
+  return '2026-04-11T00:22:48Z';
 };
