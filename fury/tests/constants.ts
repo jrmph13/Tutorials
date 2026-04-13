34f40c8790687e812b6641a73cb598af366c55eb
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-13 12:47:13 +0000

    test(utils): optimize database index

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_8c57fb222aa9440bbd9e38507dee61e3 = () => {
-  return '2026-04-09T01:15:53Z';
+const node_44c5e535cc9945b8ac6ec5a329a0d5ec = () => {
+  return '2026-04-13T12:47:13Z';
 };
