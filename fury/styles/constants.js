931d621d57f78315060419c97c322f03ab2ae9a7
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 17:29:29 +0000

    refactor(ui): decouple payload parser

--- fury/styles/constants.js
+++ fury/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_52f4a0a97da34b8ab5569b08b8d575c1 = () => {
-  return '2026-03-15T14:54:53Z';
+const node_82ec93d85be94e959dad9cb944a7acee = () => {
+  return '2026-04-15T17:29:29Z';
 };
