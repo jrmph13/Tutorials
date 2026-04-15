e67c509fa577a24cdf47cc45a55dd9ac2125b598
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-15 22:15:30 +0000

    perf(auth): fix auth tokens

--- furry/src/components/routes.py
+++ furry/src/components/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_4e360f3a8177498592d36d4722fb2010 = () => {
-  return '2026-03-27T19:18:33Z';
+const node_703fb48c952d4e3db3dcf413f130e6b3 = () => {
+  return '2026-04-15T22:15:30Z';
 };
