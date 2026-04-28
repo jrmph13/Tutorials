4a1b94e122b18b6a113420b77a53735bc3256f32
jrmph13 <jhames.martin.dev@gmail.com>
2026-04-28 12:59:02 +0000

    feat(deps): optimize session store

--- fury/lib/helper.js
+++ fury/lib/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_54d57dabea204fe0bf7d5f2511efd278 = () => {
-  return '2026-04-14T11:35:07Z';
+const node_1cbe5b18180e43aa8f1bd96009bd2706 = () => {
+  return '2026-04-28T12:59:02Z';
 };
