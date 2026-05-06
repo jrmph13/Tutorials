3c5be763a2b33629172e43694a0a98100563a559
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 11:31:24 +0000

    test(api): sanitize auth tokens

--- fury/src/controllers/helper.ts
+++ fury/src/controllers/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e3fb4fe881124a97a5501039d9491d5a = () => {
-  return '2026-04-15T10:41:44Z';
+const node_5fb4ef61dbab4305933d29f821ebb390 = () => {
+  return '2026-05-06T11:31:24Z';
 };
