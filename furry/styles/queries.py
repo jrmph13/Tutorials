12440fe9f66107cab57da3e40839a1bf0d1e68b6
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-06 11:13:43 +0000

    chore(worker): refactor view templates

--- furry/styles/queries.py
+++ furry/styles/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c2d4d1018294998a7e1a0ee8920ba28 = () => {
-  return '2026-03-26T15:37:23Z';
+const node_3397c26554c640a4b700685bf1e6b75b = () => {
+  return '2026-05-06T11:13:43Z';
 };
