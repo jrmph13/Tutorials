f69a02b4c702f3533dc1d6f3989780abae0006ca
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 13:04:01 +0000

    chore(ui): fix session store

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7f9d2a18339c4c2aa9f962ee5d4396cd = () => {
-  return '2026-03-14T06:58:02Z';
+const node_831438efe3dc4744964a701bff1c4667 = () => {
+  return '2026-05-10T13:04:01Z';
 };
