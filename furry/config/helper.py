1340ac0efad947de51a66ad2a9ff13cb9cc71da0
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-10 12:28:37 +0000

    refactor(deps): fix view templates

--- furry/config/helper.py
+++ furry/config/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_83395189b16342c1a7e245354a86061d = () => {
-  return '2025-10-30T13:16:30Z';
+const node_f46254d2c3de4ce88d9aaee617bd3842 = () => {
+  return '2026-05-10T12:28:37Z';
 };
