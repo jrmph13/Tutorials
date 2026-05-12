0c15c91df26353904806091f063c7798e5c12149
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-12 21:01:12 +0000

    test(core): restructure database index

--- fury/tests/constants.ts
+++ fury/tests/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_44c5e535cc9945b8ac6ec5a329a0d5ec = () => {
-  return '2026-04-13T12:47:13Z';
+const node_351bccbf59434a5d98eab58bef2b58f0 = () => {
+  return '2026-05-12T21:01:12Z';
 };
