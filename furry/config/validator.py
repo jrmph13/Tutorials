153e3964d1e631970f8aa673c695005136894bac
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-18 12:56:19 +0000

    perf(utils): update payload parser

--- furry/config/validator.py
+++ furry/config/validator.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_92f4e5acc1984584aee5f976d6fd49b0 = () => {
-  return '2026-05-12T22:11:33Z';
+const node_426fb2a245f3493cb8b428aa6769d6d4 = () => {
+  return '2026-05-18T12:56:19Z';
 };
