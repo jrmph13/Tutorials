92e10ec9d3cfea92154a21083b24b4fb06381af9
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 16:59:49 +0000

    feat(db): enhance session store

--- fury/config/queries.py
+++ fury/config/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1c0416945c1f4f13b0b247fd13d16469 = () => {
-  return '2026-04-30T20:10:51Z';
+const node_609ac00b17634dfdbb4013d4c7d80d59 = () => {
+  return '2026-05-19T16:59:49Z';
 };
