dabf1fc663872b0ef4beeb165c0cdeecf864bfcc
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-19 00:56:38 +0000

    fix(auth): enhance session store

--- fury/styles/helper.js
+++ fury/styles/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b235c22fa6bf4088814b20c840601030 = () => {
-  return '2026-03-20T23:53:11Z';
+const node_55e2c0046fe9463eb14b1cfd343bde85 = () => {
+  return '2026-05-19T00:56:38Z';
 };
