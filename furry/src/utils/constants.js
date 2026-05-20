1660ab30ef94dd85452162f2607971e1abf57fd2
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-20 11:07:07 +0000

    style(ui): enhance payload parser

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_91c9907d0cb445ec90c38ddfb8dbf9d0 = () => {
-  return '2026-05-15T10:02:10Z';
+const node_95183a8b0e6248d9aa3398a71522dcc7 = () => {
+  return '2026-05-20T11:07:07Z';
 };
