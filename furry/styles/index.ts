3424f35686e9a35b01b98a4860651c6e9fdbe039
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-21 05:11:59 +0000

    fix(deps): sanitize logging interface

--- furry/styles/index.ts
+++ furry/styles/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_25cc9a9bff304861832771bf7ca0bc0d = () => {
-  return '2026-04-22T17:26:08Z';
+const node_8d434a46e9fd4fdfb138ec68ecd5c357 = () => {
+  return '2026-05-21T05:11:59Z';
 };
