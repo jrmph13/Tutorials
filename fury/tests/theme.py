d948a8b31400d3bac39203f81148d6d514acd6fd
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-21 11:20:30 +0000

    feat(config): fix payload parser

--- fury/tests/theme.py
+++ fury/tests/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_22bfe1510fd64dabb73ae6455196c9af = () => {
-  return '2026-05-05T23:44:40Z';
+const node_f296bc712c394852b2ba5d043bc480cd = () => {
+  return '2026-05-21T11:20:30Z';
 };
