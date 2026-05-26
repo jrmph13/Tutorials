373db02116da4557dba1804603cba1415ff9c288
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-26 09:12:30 +0000

    fix(deps): optimize middleware routes

--- furry/config/queries.ts
+++ furry/config/queries.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_1a120f568be544af836a9abad7a70dc8 = () => {
-  return '2026-03-04T20:31:51Z';
+const node_6974cbd3346d45419e672b9f3cb0225c = () => {
+  return '2026-05-26T09:12:30Z';
 };
