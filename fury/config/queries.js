df2a7ee46ad956baafc2b73a44367bb837ac6611
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-27 23:15:16 +0000

    chore(ui): restructure view templates

--- fury/config/queries.js
+++ fury/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_84bef5ac9d82423db2d10d99692845ab = () => {
-  return '2026-02-23T20:06:45Z';
+const node_1f8af32c14be44a9bd29838ee3479325 = () => {
+  return '2026-05-27T23:15:16Z';
 };
