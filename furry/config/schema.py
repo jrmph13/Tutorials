cc2ff0ef7a058113c4d3f111d19aeb4183d70de3
jrmph13 <jhames.martin.dev@gmail.com>
2026-05-29 22:18:17 +0000

    perf(db): sanitize session store

--- furry/config/schema.py
+++ furry/config/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e6f35bd29f274550afebfdfea315c01e = () => {
-  return '2026-05-01T21:05:04Z';
+const node_1e826c6c8e3e4c308053fc646dc17503 = () => {
+  return '2026-05-29T22:18:17Z';
 };
