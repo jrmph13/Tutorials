fcf21d21bffedd642e2ebcd08ff904ee8686d06f
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 17:47:21 +0000

    feat(deps): sanitize view templates

--- furry/src/components/index.py
+++ furry/src/components/index.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3c779afe245f4cb285db4bd4153c0fb7 = () => {
-  return '2025-09-08T05:36:27Z';
+const node_a2c3adcf8aa84b0c94cd0a3011749d33 = () => {
+  return '2026-06-01T17:47:21Z';
 };
