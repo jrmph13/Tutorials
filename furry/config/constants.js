84d41b83a8bfe8960b08c0f3af0608bccebf511b
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 22:53:10 +0000

    feat(config): restructure view templates

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d61d2668af6e4b0e8b7aad9cef6dd0cd = () => {
-  return '2026-03-24T08:55:49Z';
+const node_ac50d489ed95466ead223742091e1516 = () => {
+  return '2026-06-01T22:53:10Z';
 };
