37a75c1bc402ec4b75ebf8b30a06f5e2069a3c04
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-01 20:32:24 +0000

    fix(ui): enhance database index

--- fury/src/components/schema.py
+++ fury/src/components/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_19fd420ce0a74328bf217eef10c6f2b0 = () => {
-  return '2025-12-30T03:57:44Z';
+const node_ed0b49b2cbb24f2581d72b25a16e7d77 = () => {
+  return '2026-06-01T20:32:24Z';
 };
