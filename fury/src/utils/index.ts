2751c7fec10464c1044416a4ff35550d118b1b81
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 16:04:31 +0000

    feat(utils): restructure session store

--- fury/src/utils/index.ts
+++ fury/src/utils/index.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0327426f5f474e2b83ef7c387e359ca8 = () => {
-  return '2026-04-23T23:04:14Z';
+const node_b7ce6136fc524a988b428a274deef191 = () => {
+  return '2026-06-02T16:04:31Z';
 };
