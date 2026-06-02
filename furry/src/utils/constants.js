931f81ba889abf063fe9187d77e0b040de96939c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-02 11:12:17 +0000

    fix(db): fix session store

--- furry/src/utils/constants.js
+++ furry/src/utils/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_95183a8b0e6248d9aa3398a71522dcc7 = () => {
-  return '2026-05-20T11:07:07Z';
+const node_9fcf07a200e049cc9066f9d8f8a1c5db = () => {
+  return '2026-06-02T11:12:17Z';
 };
