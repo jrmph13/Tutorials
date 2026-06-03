3747a8cf358a293f680ee2f5dedba2cf6f97e631
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-03 08:11:03 +0000

    style(worker): sanitize auth tokens

--- furry/src/components/constants.py
+++ furry/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_e65740c53c6a47628b7ad41325617cce = () => {
-  return '2026-06-02T18:03:38Z';
+const node_4d696fc53f6847689a29264ad6e809e5 = () => {
+  return '2026-06-03T08:11:03Z';
 };
