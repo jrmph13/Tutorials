d7d3919f60d587f2f218f079ef6f9244742ce78c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 02:14:02 +0000

    fix(ui): integrate view templates

--- furry/styles/helper.py
+++ furry/styles/helper.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_da4f789982d24e1cb29b1e80fb26a6af = () => {
-  return '2026-03-13T13:39:57Z';
+const node_2129c658dc594201886e4a80dcef4ce6 = () => {
+  return '2026-06-05T02:14:02Z';
 };
