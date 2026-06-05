56bbf63e6699c5e69389f618c0c7a490b6cbc746
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-05 23:02:36 +0000

    fix(core): decouple database index

--- furry/config/theme.py
+++ furry/config/theme.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_aff76f2a506c4e99bd016a90950cd46a = () => {
-  return '2026-06-05T06:22:16Z';
+const node_198be02d4a9d4533a74d8d99f750f385 = () => {
+  return '2026-06-05T23:02:36Z';
 };
