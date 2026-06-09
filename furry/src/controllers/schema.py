da8759c7de94f9cf1aef560dd3143b9e30e08b68
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-09 09:08:52 +0000

    chore(ui): update middleware routes

--- furry/src/controllers/schema.py
+++ furry/src/controllers/schema.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bfebc715bded415c9b6876c81b9e94c8 = () => {
-  return '2026-05-14T13:20:23Z';
+const node_ed2d5c6f9350430ab1b78b6d2457b4c8 = () => {
+  return '2026-06-09T09:08:52Z';
 };
