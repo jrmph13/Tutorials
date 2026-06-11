e081d1209cb71f4e0154273a7d0d8b91f0e94a3c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-11 19:39:14 +0000

    fix(config): decouple auth tokens

--- furry/config/helper.js
+++ furry/config/helper.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_d217eb4109dc4747a6858dc2b2f73236 = () => {
-  return '2026-05-12T13:59:59Z';
+const node_a80c1128d98e49ebbe30f961f90269d2 = () => {
+  return '2026-06-11T19:39:14Z';
 };
