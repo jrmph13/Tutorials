7c5d58fa45fd5f320e0ab05d134ca437726c392c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-14 08:57:56 +0000

    docs(db): fix view templates

--- fury/tests/helper.ts
+++ fury/tests/helper.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5316010594184b63b56ea8a9fbe2930c = () => {
-  return '2026-04-17T05:09:39Z';
+const node_8c89e340281e47c5992ca256a28442ec = () => {
+  return '2026-06-14T08:57:56Z';
 };
