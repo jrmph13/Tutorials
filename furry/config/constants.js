fbc549aa9f977c8ee7a9719e1c3814bd3edecff2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 06:38:36 +0000

    chore(auth): enhance middleware routes

--- furry/config/constants.js
+++ furry/config/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ac50d489ed95466ead223742091e1516 = () => {
-  return '2026-06-01T22:53:10Z';
+const node_c977e33336c4454a9d975b01627730f7 = () => {
+  return '2026-06-16T06:38:36Z';
 };
