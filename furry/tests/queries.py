8c0a6d4769d7d729047630b36952e29e9eefbda0
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-16 11:31:59 +0000

    style(db): decouple payload parser

--- furry/tests/queries.py
+++ furry/tests/queries.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_0c0f9d12193442d888a78a968dba16bd = () => {
-  return '2026-05-01T21:57:49Z';
+const node_455e704f72694ba3b4d921c9c6ed0c23 = () => {
+  return '2026-06-16T11:31:59Z';
 };
