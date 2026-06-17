26e7c9e383daf57c21040dd3ca4c387c363d721c
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 19:53:15 +0000

    docs(ui): optimize middleware routes

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bc03821ebc8e44819c4de3046d696e07 = () => {
-  return '2026-03-18T22:04:32Z';
+const node_a50e2fcc35a84c04a84de4d0a486b2d3 = () => {
+  return '2026-06-17T19:53:15Z';
 };
