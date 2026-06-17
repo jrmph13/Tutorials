401f760a32408968d89c6ae29760b278aefd6405
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-17 00:33:25 +0000

    chore(core): fix auth tokens

--- fury/src/components/constants.py
+++ fury/src/components/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_cbc15756c64c4f31aa73cf9ffd7e1c28 = () => {
-  return '2026-05-18T18:16:26Z';
+const node_b204534c39f94a8dbf2933bdb9b35db2 = () => {
+  return '2026-06-17T00:33:25Z';
 };
