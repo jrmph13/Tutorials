f60ccdf7e68609da2d075b435e1fc7209a7385d2
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 00:00:33 +0000

    test(deps): decouple session store

--- furry/config/queries.js
+++ furry/config/queries.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ee4346d52e0b4406ae77e3f941167400 = () => {
-  return '2026-06-09T06:28:53Z';
+const node_6173a525c0b544848e74f52183a0f009 = () => {
+  return '2026-06-18T00:00:33Z';
 };
