bd838647dde30f8525bf4cf4e3e743f039125351
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-18 15:35:00 +0000

    fix(config): sanitize validation pipeline

--- fury/config/constants.py
+++ fury/config/constants.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_bf1a4e225024484db15460fa6ee9dc2d = () => {
-  return '2026-05-11T13:07:43Z';
+const node_814009a3e7364bd3ac5d68f7700d4bd4 = () => {
+  return '2026-06-18T15:35:00Z';
 };
