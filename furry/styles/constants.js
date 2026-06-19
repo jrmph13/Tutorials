fbe96f3c3d8503931b860fc06354025a2d34b05a
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-19 00:29:56 +0000

    perf(worker): restructure middleware routes

--- furry/styles/constants.js
+++ furry/styles/constants.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_b1462da54d394bcb82d589a6e11dd834 = () => {
-  return '2026-05-06T01:01:32Z';
+const node_7561ca59ece8445ab840e4115c2f5bdc = () => {
+  return '2026-06-19T00:29:56Z';
 };
