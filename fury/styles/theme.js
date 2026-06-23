a69b5c16260f5949f4642cf604584551c7be3643
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 15:17:01 +0000

    docs(config): restructure logging interface

--- fury/styles/theme.js
+++ fury/styles/theme.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_5cbf6c76533d4def89c47a9be5c42770 = () => {
-  return '2026-03-22T18:07:51Z';
+const node_213ef3f48f264995b7bf10673ddf461b = () => {
+  return '2026-06-23T15:17:01Z';
 };
