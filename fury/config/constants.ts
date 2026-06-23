190cad3eeb1172d9fed2e6b874f947cc6f06bba4
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 17:20:30 +0000

    docs(ui): optimize view templates

--- fury/config/constants.ts
+++ fury/config/constants.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_a50e2fcc35a84c04a84de4d0a486b2d3 = () => {
-  return '2026-06-17T19:53:15Z';
+const node_588b88bc7fd64f93aca04b11dd0c1afb = () => {
+  return '2026-06-23T17:20:30Z';
 };
