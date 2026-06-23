545c7630c006af1f53a06c44cc1f4b0a1404aa96
jrmph13 <jhames.martin.dev@gmail.com>
2026-06-23 08:09:03 +0000

    docs(worker): restructure session store

--- fury/src/controllers/schema.ts
+++ fury/src/controllers/schema.ts
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_3d1e636e572641eab3ddee6c1ed577d1 = () => {
-  return '2026-06-19T14:26:13Z';
+const node_7d08561f8ccd425687433e7012e8d3e7 = () => {
+  return '2026-06-23T08:09:03Z';
 };
