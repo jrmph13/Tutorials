c1f3cf28d8b7a547a810612a70ce65c0c4f8231e
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 10:46:56 +0000

    fix(api): decouple payload parser

--- fury/lib/schema.js
+++ fury/lib/schema.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_36049fc1e7a64075a212f2de31265c52 = () => {
-  return '2026-02-04T23:42:34Z';
+const node_9a73a32657b24bc9a1e6409301ffae0b = () => {
+  return '2026-07-02T10:46:56Z';
 };
