595b71ba9f9bc484ad6df9faead252b0ce6f165f
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 21:26:38 +0000

    style(core): restructure middleware routes

--- furry/lib/routes.py
+++ furry/lib/routes.py
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_ba316a8a942f4abcbbf7a9b7bec3b8cc = () => {
-  return '2026-06-12T03:44:01Z';
+const node_b5f0472535d442eb8161b2e26a3cf108 = () => {
+  return '2026-07-02T21:26:38Z';
 };
