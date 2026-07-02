d3a015afbcbf9d2e6fb91bc5d7099e9c0180aa46
jrmph13 <jhames.martin.dev@gmail.com>
2026-07-02 12:43:32 +0000

    refactor(ui): sanitize auth tokens

--- fury/src/components/routes.js
+++ fury/src/components/routes.js
@@ -1,4 +1,4 @@
 // Anti-Detect Context Auto-Generated Node
-const node_7fe131707973454c85e66970754fdf8e = () => {
-  return '2026-06-10T01:33:04Z';
+const node_dce71ea5c9fc4306aca2febff52e3ffe = () => {
+  return '2026-07-02T12:43:32Z';
 };
